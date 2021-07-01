1f8ea7950f6766907cf73fec27e3e07d2d5c087f
naresh ch <naresh@tlvfintech.com>
2021-07-01 13:08:23 +0530

    mega menu changes

--- src/layout/header.component.js
+++ src/layout/header.component.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Layout, List, Skeleton, Menu, Modal, Typography, Row, Col, Divider, Dropdown, Avatar, Drawer, Radio, Tabs, Card, Button, Switch, Input } from 'antd';
+import { Layout, List, Skeleton, Menu, Modal, Typography, Row, Col, Divider, Dropdown, Avatar, Carousel, Drawer, Radio, Tabs, Card, Button, Switch, Input } from 'antd';
 import { RightOutlined, UserOutlined, InfoCircleFilled, CheckOutlined, ArrowLeftOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 import logoWhite from '../assets/images/logo-white.png';
@@ -230,7 +230,7 @@ class tlvHeader extends Component {
 
                     {/* <div className="mega-menu">
                     <Row gutter={[16, 16]} className="megamenu-link"   >
-                        <Col lg={6} xl={4}>
+                        <Col  xl={4}>
                             <Title className="text-white megamenu-label mb-16 fw-500">Start</Title>
                             <Paragraph className="text-white-30 fs-16 mb-24">We are a platform that connects banks, payment systems, and people.</Paragraph>
                              <div className="mobile-megalinks">
@@ -238,7 +238,7 @@ class tlvHeader extends Component {
                             <Link>Your Portfolio</Link> 
                              </div>
                         </Col>
-                        <Col lg={6} xl={5}>
+                        <Col  xl={5}>
                             <Title className="text-white megamenu-label  fw-500 mb-24">Personal</Title>
                             <Link className="pt-24"> Wallets</Link>
                           <Paragraph className="text-white-30 fs-16 mb-0">Full control of your private keys.</Paragraph>
@@ -247,7 +247,7 @@ class tlvHeader extends Component {
                             <Link>Exchange</Link>
                              </div> 
                         </Col>
-                        <Col lg={6} xl={5}>
+                        <Col  xl={5}>
                             <Title className="text-white megamenu-label  fw-500">Crypto</Title>
                             <div className="mobile-megalinks">
                             <Link >Buy and Sell</Link>
@@ -255,7 +255,7 @@ class tlvHeader extends Component {
                             <Link>Deposit and Withdraw</Link>
                              </div> 
                         </Col>
-                        <Col lg={6} xl={5} >
+                        <Col  xl={5} >
                             <Title className="text-white megamenu-label  fw-500">Business</Title>
                             <Paragraph className="text-white-30 fs-16">User can create a separate account such as a corporate segregated wallet system.</Paragraph>
                             <div className="mobile-megalinks">
@@ -263,18 +263,18 @@ class tlvHeader extends Component {
                             <Link>Mass Payments</Link>
                              </div> 
                         </Col>
-                        <Col lg={6} xl={5} />
+                        <Col  xl={5} />
                     </Row>
                     <Row gutter={[16, 16]} className="megamenu-link mobile-none ">
-                    <Col lg={6} xl={4}>
+                    <Col  xl={4}>
                             <Link>The Dashboard</Link>
                             <Link>Your Portfolio</Link>
                         </Col>
-                        <Col lg={6} xl={5}>
+                        <Col  xl={5}>
                             <Link>Cards</Link>
                             <Link>Exchange</Link>
                         </Col>
-                        <Col lg={6} xl={5}>
+                        <Col  xl={5}>
                            
                         <Link >Buy and Sell</Link>
                             <Link>Swap Services</Link>
@@ -311,34 +311,108 @@ class tlvHeader extends Component {
                             <Link>Contact Us</Link>
                             <Link >Sign In</Link>
                         </Col>
-                        <Col lg={6} xl={5} />
+                        <Col  xl={5} />
                     </Row>
                     </div> */}
 
                     {/* mega menu login after */}
-
+                    <Carousel  className="mb-24">
+                    <div className="mega-menu">
+                    <Row gutter={[16, 16]} className="megamenu-link"   >
+                        <Col  xl={5}>
+                            <Title className="text-white megamenu-label mb-16 fw-500">Start</Title>
+                            <Paragraph className="text-white-30 fs-16 mb-24 fw-400">We are a platform that connects banks, payment systems, and people.</Paragraph>
+                             <div className="">
+                             <Link>The Dashboard</Link>
+                            <Link>Your Portfolio</Link> 
+                             </div>
+                        </Col>
+                        <Col  xl={4}>
+                            <Title className="text-white megamenu-label  fw-500 mb-24">Personal</Title>
+                            <Link className="pt-24"> Wallets</Link>
+                          <Paragraph className="text-white-30 fs-16 mb-0 fw-400">Full control of your private keys.</Paragraph>
+                          <div className="">
+                            <Link>Cards</Link>
+                            <Link>Exchange</Link>
+                             </div> 
+                        </Col>
+                        <Col  xl={6}>
+                            <Title className="text-white megamenu-label  fw-500">Crypto</Title>
+                            <div className="">
+                            <Link >Buy and Sell</Link>
+                            <Link>Swap Services</Link>
+                            <Link>Deposit and Withdraw</Link>
+                             </div> 
+                        </Col>
+                        <Col  xl={1}></Col>
+                        <Col  xl={6} >
+                            <Title className="text-white megamenu-label  fw-500">Business</Title>
+                            <Paragraph className="text-white-30 fs-16 fw-400">User can create a separate account such as a corporate segregated wallet system.</Paragraph>
+                            <div className="">
+                            <Link>Corporate Wallet</Link>
+                            <Link>Mass Payments</Link>
+                             </div> 
+                        </Col>
+                        <Col  xl={2} />
+                    </Row>
+                    <Divider className="megamenu-divider mobile-none" />
+                  <Row gutter={[16, 16]} className="megamenu-link">
+                       
+                        <Col lg={5} xl={5}>
+                           
+                        </Col>
+                        <Col lg={5} xl={4}>
+                           
+                        </Col>
+                        <Col lg={7} xl={6}>
+                        <Title className="fs-36 text-white megamenu-label  mb-16 fw-500">Connect</Title>
+                            <Link>Meet Our Team</Link>
+                            <Link>Report A Bug</Link>
+                            <Link>FAQ</Link>
+                            <Link>Contact Us</Link>
+                            <Link >Sign In</Link>
+                           
+                        </Col>
+                        <Col xl={1} />
+                        <Col lg={5} xl={6}>
+                        <Title className="fs-36 text-white mb-16 fw-500 megamenu-label ">Security</Title>
+                            <Paragraph className="text-white fs-14 fw-400">CURRENT SECURITY LEVEL<br /><span className="text-green fw-700">Medium</span></Paragraph>
+                            <Paragraph className="text-white fs-14 fw-400">Your account has security features switched off, leaving it potentially vulnerable to specific attacks. Set up these security features to improve the security of your account.</Paragraph>
+                        </Col>
+                        <Col  xl={2} />
+                    </Row>
+                    </div> 
                     <div className="mega-menu">
                         <Row gutter={16} className="megamenu-link">
-                            <Col lg={6} xl={5}>
+                            <Col  xl={5}>
+                            <div className="wrapper">
+                                <div className="item-wrapper">
                                 <Title className="text-white megamenu-label mb-16 fw-500">Preferences</Title>
                                 <Avatar size={60} style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} />
                                 <Paragraph className="text-white-30 fs-16 mb-0">Michael Quiapos</Paragraph>
                                 <Paragraph className="text-secondary fs-14">Great. i will have a look...</Paragraph>
+                                </div>
+                           </div>
                             </Col>
-                            <Col lg={6} xl={4}>
-                                <Title className="text-white megamenu-label  fw-500 mb-24">Wallet</Title>
-                                <div className="mobile-megalinks">
+                            <Col  xl={4}>
+                                <div className="wrapper">
+                                <Title className="text-white megamenu-label  fw-500 mb-24 item-wrapper">Wallet</Title>
+                                <div className="item-wrapper1">
                                     <Link>Address Book</Link>
                                     <Link>Invite Friends</Link>
                                     <Link>Buy Crypto</Link>
                                     <Link>Light Theme <Switch onChange={this.onChange} size="small" className="custom-toggle" /></Link>
                                 </div>
+                                </div>
                             </Col>
 
-                            <Col lg={6} xl={5}>
+                            <Col  xl={6}>
+                            <div className="wrapper">
+                                <div className="item-wrapper">
                                 <Title className="text-white megamenu-label  fw-500">Localization</Title>
                                 <Paragraph className="text-white-30 fs-16 fw-400">User can create a separate account such as a corporate segregated wallet system.</Paragraph>
-                                <div className="mobile-megalinks">
+                              </div>
+                                <div className="item-wrapper1">
                                     <div className="d-flex justify-content">
                                         <p className="text-white-30 fs-18 mb-0">Language</p>
                                         <p className="text-white-30 fs-18 mb-0">lang</p>
@@ -348,60 +422,35 @@ class tlvHeader extends Component {
                                         <p className="text-white-30 fs-18 mb-0"> USD</p>
                                     </div>
                                 </div>
+                                </div>
                             </Col>
-                            <Col lg={6} xl={1} className=" mobile-none " />
-                            <Col lg={6} xl={5} >
+                            <Col  xl={1} className=" mobile-none " />
+                            <Col  xl={6} >
                                 <Title className="text-white megamenu-label  fw-500">Support</Title>
-                                <div className="mobile-megalinks">
+                                <div >
                                     <Link>Help Center</Link>
                                     <Link>About</Link>
                                     <Link>Social Networks</Link>
                                 </div>
                             </Col>
-                            <Col lg={6} xl={5} className=" mobile-none " />
-                        </Row>
-                        <Row gutter={[16, 16]} className="megamenu-link mobile-none ">
-                            <Col lg={6} xl={5} className=" mobile-none ">
-                            </Col>
-                            <Col lg={6} xl={4}>
-                                <Link>Address Book</Link>
-                                <Link>Invite Friends</Link>
-                                <Link>Buy Crypto</Link>
-                                <Link>Light Theme <Switch onChange={this.onChange} size="small" className="custom-toggle" /></Link>
-                            </Col>
-                            <Col lg={6} xl={5}>
-                                <div className="d-flex justify-content">
-                                    <p className="text-white-30 fs-18 mb-0">Language</p>
-                                    <p className="text-white-30 fs-18 mb-0">lang</p>
-                                </div>
-                                <div className="d-flex justify-content">
-                                    <p className="text-white-30 fs-18 mb-0">Currency</p>
-                                    <p className="text-white-30 fs-18 mb-0"> USD</p>
-                                </div> </Col>
-                            <Col lg={6} xl={1} />
-                            <Col lg={6} xl={5} >
-                                <Link>Help Center</Link>
-                                <Link>About</Link>
-                                <Link>Social Networks</Link>
-                            </Col>
-                            <Col lg={6} xl={4} className=" mobile-none " />
+                            <Col  xl={2} className=" mobile-none " />
                         </Row>
                         <Divider className="megamenu-divider mobile-none" />
                         <Row gutter={[16, 16]} className="megamenu-link "  >
-                            <Col lg={6} xl={4} className=" mobile-none ">
+                            <Col  xl={4} className=" mobile-none ">
 
                             </Col>
-                            <Col lg={6} xl={5} className="pt-24 mobile-none">
+                            <Col  xl={5} className="pt-24 mobile-none">
 
                             </Col>
-                            <Col lg={6} xl={5}>
+                            <Col  xl={5}>
                                 <Title className=" text-white mb-16  megamenu-label  fw-500">Connect</Title>
                                 <Link>Report A Bug</Link>
                                 <Link>FAQ</Link>
                                 <Link>Chat</Link>
                             </Col>
 
-                            <Col lg={6} xl={5} >
+                            <Col  xl={5} >
                                 <Title className="fs-36 text-white mb-16  megamenu-label fw-500 ">Security</Title>
                                 <Paragraph className="text-white fs-16">CURRENT SECURITY LEVEL<br /><span className="text-green fw-700">Medium</span></Paragraph>
                                 <Link>Backup Wallet</Link>
@@ -410,9 +459,10 @@ class tlvHeader extends Component {
                                 <Link>Activate face ID <Switch defaultChecked onChange={this.onChange} size="small" className="custom-toggle" /></Link>
                                 <Link>Activate biometry <Switch defaultChecked onChange={this.onChange} size="small" className="custom-toggle" /></Link>
                             </Col>
-                            <Col lg={6} xl={5} />
+                            <Col  xl={5} />
                         </Row>
                     </div>
+                    </Carousel>
                 </Modal>
                 <Drawer
                     title={[<div className="side-drawer-header"><ArrowLeftOutlined className="text-white c-pointer" onClick={this.closeBuyDrawer} />
