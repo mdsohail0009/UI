5f899a333146e5317844a1e2a2d1daece6bf4441
rao <manoj@zirafftechnologies.com>
2021-07-03 15:41:21 +0530

    form fiat toggle issue

--- src/components/buysell.component/depositCrypto.js
+++ src/components/buysell.component/depositCrypto.js
@@ -3,10 +3,7 @@ import { Drawer, Typography, Button, Radio, Tabs, List } from 'antd';
 import config from '../../config/config';
 import { setStep } from '../../reducers/buysellReducer';
 import { connect } from 'react-redux';
-const depostOptions = [
-    { label: 'From Crypto', value: 'From Crypto' },
-    { label: 'From Fiat', value: 'From Fiat' },
-];
+
 class depositCrypto extends Component {
     state = {
         depositToggle: 'From Crypto'
@@ -24,19 +21,21 @@ class depositCrypto extends Component {
                     <Radio.Button value={1}>From Crypto</Radio.Button>
                     <Radio.Button value={2}>From Fiat</Radio.Button>
                 </Radio.Group>
-                <List onClick={() => this.props.changeStep('step8')}
-                    itemLayout="horizontal"
-                    dataSource={config.tlvCoinsList}
-                    className="wallet-list"
-                    renderItem={item => (
-                        <List.Item>
-                            <List.Item.Meta
-                                avatar={<span className={`coin ${item.coin} mr-4`} />}
-                                title={<div className="fs-16 fw-600 text-upper text-white-30 mb-0 mt-12">{item.coin}</div>}
-                            />
-                        </List.Item>
-                    )}
-                />
+                <div className="sellcrypto-container">
+                    <List onClick={() => this.props.changeStep('step8')}
+                        itemLayout="horizontal"
+                        dataSource={config.tlvCoinsList}
+                        className="wallet-list"
+                        renderItem={item => (
+                            <List.Item>
+                                <List.Item.Meta
+                                    avatar={<span className={`coin ${item.coin} mr-4`} />}
+                                    title={<div className="fs-16 fw-600 text-upper text-white-30 mb-0 mt-12">{item.coin}</div>}
+                                />
+                            </List.Item>
+                        )}
+                    />
+                </div>
             </>
         );
     }
--- src/components/buysell.component/depositFiat.js
+++ src/components/buysell.component/depositFiat.js
@@ -6,10 +6,6 @@ import CryptoList from '../shared/cryptolist';
 import { Link } from "react-router-dom";
 import Translate from 'react-translate-component';
 
-const depostOptions = [
-    { label: 'From Crypto', value: 'From Crypto' },
-    { label: 'From Fiat', value: 'From Fiat' },
-];
 class DepositFiat extends Component {
     state = {
         buyToggle: 'From Fiat'
@@ -23,22 +19,22 @@ class DepositFiat extends Component {
         return (
             <>
                 <Radio.Group
-                    options={depostOptions}
-                    onChange={this.handleDepositToggle}
-                    value={this.state.buyToggle}
-                    optionType="button"
-                    buttonStyle="solid"
-                    size="large"
-                    className="buysell-toggle"
-                />
-                <WalletList />
-                <Translate className="mb-0 mt-36 fs-14 text-white fw-500 text-upper" content="wire_transfer_mthd" component={Paragraph} />
-                <div className="d-flex align-center mt-16 c-pointer" onClick={this.wiriTransfer}>
-                    <span className="coin btc" />
-                    <div className="ml-16"><Paragraph className="mb-0 fs-14 text-white-30 fw-300">Fidor Bank AG</Paragraph>
-                        <Paragraph className="mb-0 fs-12 text-white-30 fw-300">EUR</Paragraph>
+                    defaultValue={1}
+                    onChange={this.handleBuySellToggle}
+                    className="buysell-toggle crypto-toggle">
+                    <Radio.Button value={1}>From Crypto</Radio.Button>
+                    <Radio.Button value={2}>From Fiat</Radio.Button>
+                </Radio.Group>
+                <div className="sellcrypto-container">
+                    <WalletList />
+                    <Translate className="mb-0 mt-36 fs-14 text-white fw-500 text-upper" content="wire_transfer_mthd" component={Paragraph} />
+                    <div className="d-flex align-center mt-16 c-pointer" onClick={this.wiriTransfer}>
+                        <span className="coin btc" />
+                        <div className="ml-16"><Paragraph className="mb-0 fs-14 text-white-30 fw-300">Fidor Bank AG</Paragraph>
+                            <Paragraph className="mb-0 fs-12 text-white-30 fw-300">EUR</Paragraph>
+                        </div>
+                        <Translate className="recomnd-tag fs-12 text-white-30 fw-300 ml-auto" content="recommended" component={Text} />
                     </div>
-                    <Translate className="recomnd-tag fs-12 text-white-30 fw-300 ml-auto" content="recommended" component={Text} />
                 </div>
             </>
         )
