7ac7075f4f7873221d448896fba5beef5138ec5d
naresh ch <naresh@tlvfintech.com>
2021-07-07 18:10:26 +0530

    confirmation msg

--- src/components/shared/confirmation.js
+++ src/components/shared/confirmation.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
 import confirm from '../../assets/images/confirm.png';
+import success from '../../assets/images/success.png';
 import { Drawer, Typography, Button, Card, Input, Tooltip, Space } from 'antd';
 import { Link } from 'react-router-dom';
 import Translate from 'react-translate-component';
-
+import SuccessMsg from './success';
+const { Title, Paragraph, Text } = Typography;
 class ConfirmMsg extends Component {
+    state={
+        success:false,
+    }
+     componentDidMount() {
+        setTimeout(() => {
+          this.setState({success:true})
+        }, 4000)
+      }
     render() {
-        const { Title, Paragraph, Text } = Typography;
         return (
-            <>
-                <div className="success-pop text-center">
+            <> 
+            {!this.state.success ? <div className="success-pop text-center">
                     <img src={confirm} className="confirm-icon" />
                     <Translate content="confirm_msg" component={Title} className="text-white-30 fs-36 fw-200 mb-4" />
                     <Translate content="confirm_text" component={Paragraph} className="fs-16 text-white-30 fw-200" />
-                </div>
+                </div> : 
+                <SuccessMsg/>
+                 }
             </>
         );
     }
