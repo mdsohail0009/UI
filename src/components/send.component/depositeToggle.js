ca773d8d48ec3396736b356916f9539cee98d80b
rao <manoj@zirafftechnologies.com>
2021-07-09 14:37:32 +0530

    deposit fiat

--- src/components/send.component/depositeToggle.js
+++ src/components/send.component/depositeToggle.js
@@ -89,7 +89,7 @@ class DepositeCrypto extends Component {
                     :
                     <>
                         <Translate content="deposite_a_crypto" component={Title} className="text-white-30 fs-36 fw-200 mb-16" />
-                        <Translate content="deposite_a_cryto_txt" component={Paragraph} className="fs-16 text-secondary" />
+                        <Translate content="deposite_a_cryto_txt" component={Paragraph} className="fs-16 text-white-30 fw-200" />
                         <div className="sellcrypto-container auto-scroll">
                             <List
                                 itemLayout="horizontal"
--- src/components/send.component/withdrawScan.js
+++ src/components/send.component/withdrawScan.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Typography, Button } from 'antd';
+import { Typography, Button, message } from 'antd';
 import { Link } from 'react-router-dom';
 import { setStep } from '../../reducers/sendreceiveReducer';
 import { connect } from 'react-redux';
@@ -8,6 +8,9 @@ import Translate from 'react-translate-component';
 
 class WithdrawScan extends Component {
     state = {}
+    success = () => {
+        message.success('Address was copied!');
+    };
     render() {
         const { Paragraph, Text } = Typography;
         return (
@@ -15,13 +18,13 @@ class WithdrawScan extends Component {
                 <div className="scanner-img">
                     <img src={sacnner} />
                 </div>
-                <div className="crypto-address">
+                <div className="crypto-address mt-36">
                     <Translate className="mb-0 fw-400 text-secondary" content="address" component={Text} />
                     <div className="mb-0 fs-14 fw-500 text-textDark">TAQgcJD9p29m77EnXweijpHegPUSnxkdQW</div>
                 </div>
                 <Translate className="text-center f-12 text-white fw-200" content="address_hint_text" component={Paragraph} />
-                <Translate size="large" block className="pop-btn" style={{ marginTop: '100px' }} content="copy" component={Button} />
-                <Translate type="text" size="large" className="text-center text-white-30 pop-cancel fw-400 text-captz text-center" block content="cancel" onClick={()=>this.props.changeStep('step1')} component={Button} />
+                <Translate size="large" block className="pop-btn" style={{ marginTop: '100px' }} content="copy" component={Button} onClick={this.success} />
+                <Translate type="text" size="large" className="text-center text-white-30 pop-cancel fw-400 text-captz text-center" onClick={() => this.props.changeStep('step1')} block content="share" component={Button} />
             </div>
         )
     }
