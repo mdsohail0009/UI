7199cf33ed1fb616cff3bef4e65d300e06a18ef9
rao <manoj@zirafftechnologies.com>
2021-07-10 12:07:36 +0530

    deposit withdraw responsive issues

--- src/components/buysell.component/selectCrypto.js
+++ src/components/buysell.component/selectCrypto.js
@@ -29,7 +29,7 @@ class SelectCrypto extends Component {
                     </span>
                     <div className="crypto-details">
                         <Text className="crypto-percent text-white fw-700">25<sup className="fs-24 text-white fw-700" style={{ verticalAlign: 'Middle', marginLeft: 14 }}>%</sup></Text>
-                        <div className="fs-16 text-white-30 fw-200 text-right">
+                        <div className="fs-16 text-white-30 fw-200 crypto-amount">
                             <div>1.0147668 ETH</div>
                             <div>$ 41.07</div>
                         </div>
