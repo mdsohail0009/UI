b5c7dfa38eb2eb184758666ec77325cbb9eefd2b
sowjanya ponnam <sowjanya@tlvfintech.com>
2021-07-26 19:44:10 +0530

    design changes

--- src/components/swap.component/swapCoins.js
+++ src/components/swap.component/swapCoins.js
@@ -113,7 +113,7 @@ class SwapCoins extends Component {
                     </div>
                     <span className="icon swapfrom-arrow c-pointer" onClick={()=>this.swapingCurr()}></span>
                     <div className="mt-16 swap-updown" onClick={this.swapingCurr}>
-                            <span className="icon sm uparw-o-white d-block c-pointer" />
+                            <span className="icon sm uparw-o-white d-block c-pointer mb-4" />
                             <span className="icon sm dwnarw-o-white d-block c-pointer" />
                     </div>
                 </div>
