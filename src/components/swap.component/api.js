5decceeb235e3848f0aa605dd199477ed2d81918
subbareddy <subbaiah@tlvfintech.com>
2021-07-27 11:10:32 +0800

    error msg toaster style implemented

--- src/components/swap.component/api.js
+++ src/components/swap.component/api.js
@@ -1,10 +1,8 @@
 import { apiClient } from '../../api';
 import { ApiControllers } from '../../api/config'
 const Portfolio = "Exchange/";
-const memId = "E3BF0F02-70E5-4575-8552-F8C49533B7C6";
 
 const getportfolio = (member_id) => {
-debugger;
     return apiClient.get(Portfolio + `MemberCrypto?memberId=${member_id}`);
 }
 const fetchCurrConvertionValue = (from, to, value) => {
