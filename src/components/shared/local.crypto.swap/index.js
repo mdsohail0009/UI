cb6e8413840832c532950abb6d170616f87bf670
subbareddy <subbareddy@zirafftechnologies.com>
2021-09-02 12:55:20 +0530

    restriction to 2 decimals

--- src/components/shared/local.crypto.swap/index.js
+++ src/components/shared/local.crypto.swap/index.js
@@ -44,7 +44,8 @@ const LocalCryptoSwapper = (props, ref) => {
         <div className="enter-val-container">
             <Text className="fs-30 fw-100 text-white-30 text-defaultylw mr-4">{!isSwaped ? localCurrency : cryptoCurrency}</Text>
             <NumberFormat className="fw-100 text-white-30 text-center enter-val p-0" maxLength={25} customInput={Input} thousandSeparator={true} prefix={isSwaped ? "" : "$"}
-                placeholder="0.00"
+               decimalScale={2}
+               placeholder="0.00"
                 bordered={false}
                 style={{ lineHeight: '48px', fontSize: 30, paddingRight: '40px !important' }}
                 onKeyPress={(e) => {
