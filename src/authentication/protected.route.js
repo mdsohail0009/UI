43edf8fb1271d66b086313e093116bfa7f958428
subbareddy <subbareddy@zirafftechnologies.com>
2021-09-06 19:35:19 +0530

    scroll

--- src/authentication/protected.route.js
+++ src/authentication/protected.route.js
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { Route, withRouter } from 'react-router-dom';
 import connectStateProps from '../utils/state.connect';
 const ProtectedRoute = ({ component: Component, ...rest }) => {
-    window.scrollTo(0, 0)
     useEffect(() => {
     }, [])
     return (
