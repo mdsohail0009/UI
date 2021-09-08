075e9c22a001f1912cd72bbb23a3969d13af034f
Naresh <naresh@tlvfintech.com>
2021-09-08 16:29:12 +0530

    setting

--- src/components/userProfile.component/userProfile.js
+++ src/components/userProfile.component/userProfile.js
@@ -3,21 +3,23 @@ import { Row, Col } from 'antd'
 import ProfileInfo from './profileInfo';
 import Security from './security'
 import Translate from 'react-translate-component';
-
+import { userManager } from '../../authentication';
+import Settings from './settings';
 
 class userProfile extends Component {
     state={
        isProfile:false,
        isSecurity:false,
+       isSetting:false
     }
     handleProfile=() =>{
          this.setState({isProfile:true})
     }
     handleSecurity=() =>{
-        this.setState({isSecurity:true})
+        this.setState({isSecurity:true,isProfile:false })
     }
     handleSetting=() =>{
-        
+        this.setState({isSetting:true,isProfile:false })
     }
     handleAbout=() =>{
         
@@ -56,7 +58,7 @@ class userProfile extends Component {
                                     <p className="mb-0 ml-8">About</p>
                                 </div>
                             </li>
-                            <li className="user-labels mb-24"  onClick={this.handleLogout}>
+                            <li className="user-labels mb-24" onClick={() => userManager.signoutRedirect()}>
                                 <div className="d-flex">
                                 <span className="icon lg info-icon" />
                                     <p className="mb-0 ml-8">Logout</p>
@@ -71,6 +73,7 @@ class userProfile extends Component {
                         <div className="right-panel">
                             {this.state.isProfile  &&<ProfileInfo/>}
                             {this.state.isSecurity && <Security/>} 
+                            {this.state.isSetting && <Settings/>}
                         </div>
                     </Col>
                 </Row>
