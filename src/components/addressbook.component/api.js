e17eef200706dee4f1d51026a381936742ce4ca8
rao <rao@tlvfintech.com>
2021-09-21 19:55:25 +0530

    coins list

---
+++ src/components/addressbook.component/api.js
@@ -0,0 +1,8 @@
+import { apiClient } from '../../api';
+import { ApiControllers } from '../../api/config'
+const Portfolio = "Exchange/";
+
+const getCoinList = (member_id) => {
+    return apiClient.get(ApiControllers.exchange+`MemberCoins?memberId=${member_id}`);
+}
+export {getCoinList}
