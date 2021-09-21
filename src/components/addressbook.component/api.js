58601d486d1d3c1162fe0defe2c67ee9895394a2
Naresh <naresh@tlvfintech.com>
2021-09-21 21:01:32 +0530

    changes

--- src/components/addressbook.component/api.js
+++ src/components/addressbook.component/api.js
@@ -4,8 +4,6 @@ import { ApiControllers } from '../../api/config';
 const favouriteAddress = () => {
     return apiClient.get(ApiControllers.exchange + "RequestFavourite");
 }
-import { ApiControllers } from '../../api/config'
-const Portfolio = "Exchange/";
 
 const getCoinList = (member_id) => {
     return apiClient.get(ApiControllers.exchange+`MemberCoins?memberId=${member_id}`);
