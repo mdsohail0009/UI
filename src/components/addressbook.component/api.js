fa5a8a696d17c426169528d76ba3282d78d4aebd
Naresh <naresh@tlvfintech.com>
2021-09-23 00:00:02 +0530

    call added

--- src/components/addressbook.component/api.js
+++ src/components/addressbook.component/api.js
@@ -7,6 +7,9 @@ const favouriteFiatAddress = (member_id, type) => {
 const detailsAddress = (id) => {
     return apiClient.get(ApiControllers.exchange + `RequestFavourite?id=${id}` );
 }
+const favouriteNameCheck = (member_id, name) => {
+    return apiClient.get(ApiControllers.exchange + `FavouriteNameCheck?memberId=${member_id}&name=${name}`);
+}
 const saveAddress = (obj) => {
     return apiClient.post(ApiControllers.exchange + `AddFavourite`, obj);
 }
@@ -16,4 +19,4 @@ const getCoinList = (member_id) => {
 const getSelectedCoinDetails = (coin_code, member_id) => {
     return apiClient.get(ApiControllers.exchange + `MemberCoinDetail?memberId=${member_id}&coin=${coin_code}`)
 }
-export {getCoinList,favouriteFiatAddress, saveAddress,getSelectedCoinDetails,detailsAddress}
+export {getCoinList,favouriteFiatAddress, saveAddress,getSelectedCoinDetails,detailsAddress,favouriteNameCheck}
