ab2c9b41c3eb55afc7788d7cce652b733f4c1f0d
rao <manoj@zirafftechnologies.com>
2021-10-05 21:09:28 +0530

    notices slider design changes

--- src/components/dashboard.component/notices.js
+++ src/components/dashboard.component/notices.js
@@ -25,10 +25,10 @@ const Notices = ({ userProfile }) => {
         return <Alert type="error" message="Alert" description="Something went wrong please try again!" showIcon />
     }
     return <Carousel autoplay className="mb-24">
+
         {notices.map((notice, indx) => <div key={indx} className="p-28 carousel-card">
             <Title className="fs-24 text-black mb-4" >{notice.title}</Title>
-            <div className="fs-16 text-black mb-24" dangerouslySetInnerHTML={{ __html: notice.htmlContent }}>
-
+            <div className="fs-16 text-black tinycontent" dangerouslySetInnerHTML={{ __html: notice.htmlContent }}>
             </div>
         </div>)}
         {notices.length === 0 && <div className="carousel-card"><Text className="notice-nodata fs-20 fw-500 text-primary">Notices are not available</Text></div>}
