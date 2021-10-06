2dcbd3237e56042d564195d9ff6dd91daf23f077
Manoj kumar <manoj.ziraff@gmail.com>
2021-10-06 17:31:14 +0530

    withdraw popup issues

--- src/components/send.component/withdrawSummary.js
+++ src/components/send.component/withdrawSummary.js
@@ -17,38 +17,38 @@ const LinkValue = (props) => {
         <Translate className="text-defaultylw textpure-yellow text-underline c-pointer"
             content={props.content}
             component={Link}
-            onClick={()=>window.open("https://www.iubenda.com/terms-and-conditions/42856099",'_blank')}
+            onClick={() => window.open("https://www.iubenda.com/terms-and-conditions/42856099", '_blank')}
         />
     )
 }
 class WithdrawSummary extends Component {
     state = {
-        onTermsChange:false,
-        isButtonLoad:false,
-        usdAmount:0,
-        OneusdAmount:0,
-        errorMsg:false
+        onTermsChange: false,
+        isButtonLoad: false,
+        usdAmount: 0,
+        OneusdAmount: 0,
+        errorMsg: false
     }
-    componentDidMount(){
+    componentDidMount() {
         this.loadOneCoinData();
         this.loadData();
     }
-    loadData = async() =>{
-        const value = await convertCurrency({ from: this.props.sendReceive.withdrawCryptoObj?.walletCode, to: "USD", value: this.props.sendReceive.withdrawCryptoObj?.totalValue, isCrypto: false ,memId:this.props.userProfile.id,screenName:''})
-        this.setState({...this.state,usdAmount:value})
+    loadData = async () => {
+        const value = await convertCurrency({ from: this.props.sendReceive.withdrawCryptoObj?.walletCode, to: "USD", value: this.props.sendReceive.withdrawCryptoObj?.totalValue, isCrypto: false, memId: this.props.userProfile.id, screenName: '' })
+        this.setState({ ...this.state, usdAmount: value })
     }
-    loadOneCoinData = async() =>{
-        const value = await convertCurrency({ from: this.props.sendReceive.withdrawCryptoObj?.walletCode, to: "USD", value: 1, isCrypto: false ,memId:this.props.userProfile.id,screenName:''})
-        this.setState({...this.state,OneusdAmount:value})
+    loadOneCoinData = async () => {
+        const value = await convertCurrency({ from: this.props.sendReceive.withdrawCryptoObj?.walletCode, to: "USD", value: 1, isCrypto: false, memId: this.props.userProfile.id, screenName: '' })
+        this.setState({ ...this.state, OneusdAmount: value })
     }
-    onRefresh = () =>{
+    onRefresh = () => {
         this.loadOneCoinData();
         this.loadData();
     }
-    onCancel = () =>{
-        this.props.changeStep('withdraw_crypto_selected');
+    onCancel = () => {
+        this.props.changeStep('step1');
     }
-    onClick = async() =>{
+    onClick = async () => {
         if (this.state.onTermsChange) {
             if (this.props.userProfile.isBusiness) {
                 let saveObj = this.props.sendReceive.withdrawCryptoObj;
@@ -69,7 +69,7 @@ class WithdrawSummary extends Component {
                 this.props.changeStep('withdraw_crypto_liveness');
             }
         } else {
-            this.setState({...this.state, errorMsg:'Please agree to all Term of Use'})
+            this.setState({ ...this.state, errorMsg: 'Please agree to all Term of Use' })
         }
     }
     render() {
@@ -77,7 +77,7 @@ class WithdrawSummary extends Component {
         const link = <LinkValue content="terms_service" />;
         return (
             <>
-            {/* {!error?.valid && <Alert showIcon type="info" message={error?.title || "Buy crypto"} description={error?.message} closable onClose={() => onErrorClose ? onErrorClose() : ""} />} */}
+                {/* {!error?.valid && <Alert showIcon type="info" message={error?.title || "Buy crypto"} description={error?.message} closable onClose={() => onErrorClose ? onErrorClose() : ""} />} */}
                 <div className="cryptosummary-container auto-scroll">
                     <div className="fs-36 text-white-30 fw-200 text-center" style={{ lineHeight: '36px' }}><Currency prefix={""} decimalPlaces={8} defaultValue={this.props.sendReceive.withdrawCryptoObj?.totalValue} suffixText={this.props.sendReceive.withdrawCryptoObj?.walletCode} /> </div>
                     <div className="text-white-50 fw-300 text-center fs-14 mb-16"><Currency defaultValue={this.state.usdAmount} prefix={""} decimalPlaces={8} type={'text'} prefixText={'USD'} /></div>
@@ -85,26 +85,28 @@ class WithdrawSummary extends Component {
                         <Translate className="fw-400 text-white" content="exchange_rate" component={Text} />
                         <Currency defaultValue={this.state.OneusdAmount} decimalPlaces={8} prefix={""} className="fw-300 text-white-30" prefixText={`1 ${this.props.sendReceive.withdrawCryptoObj?.walletCode} = ${'USD'}`}
                         />
-                        
+
                     </div>
                     <div className="pay-list fs-14">
                         <Translate className="fw-400 text-white" content="amount" component={Text} />
                         <Currency prefix={""} className={'text-white'} decimalPlaces={8} defaultValue={this.props.sendReceive.withdrawCryptoObj?.totalValue} suffixText={this.props.sendReceive.withdrawCryptoObj?.walletCode} />
                     </div>
-
+                    <div className="pay-list fs-14">
+                        <Translate className="fw-400 text-white" content="address" component={Text} />
+                        <Text className="fw-400 text-white">{this.props.sendReceive.withdrawCryptoObj?.toWalletAddress}</Text>
+                    </div>
                     <div className="fs-12 text-white-30 text-center my-16">Your final amount might be changed with in
                         10 seconds.</div>
-                        <div className="p-16 mt-16 mb-0 text-center">
-                    <Translate className="fs-16 text-white-30 text-center mb-0" content="withdraw_to" component={Paragraph} />
-                    <div className={'text-white'}>{this.props.sendReceive.withdrawCryptoObj?.walletCode} Coin Address</div>
-                    <Paragraph className="fs-16 text-white-30 text-center mb-0">
-                        {this.props.sendReceive.withdrawCryptoObj?.toWalletAddress}
-                    </Paragraph>
-                </div>
+                    {/* <div className="p-16 mt-16 mb-0 text-center">
+                        <Translate className="fs-16 text-white-30 text-center mb-0" content="withdraw_to" component={Paragraph} />
+                        <div className={'text-white'}>{this.props.sendReceive.withdrawCryptoObj?.walletCode} Coin Address</div>
+                        <Paragraph className="fs-16 text-white-30 text-center mb-0">
+                            {this.props.sendReceive.withdrawCryptoObj?.toWalletAddress}
+                        </Paragraph>
+                    </div> */}
                     <div className="d-flex p-16 mb-36 agree-check">
                         <label>
-                        
-                            <input type="checkbox" id="agree-check" onChange={({ currentTarget: { checked } }) => { this.setState({onTermsChange:checked?true:false}) }} />
+                            <input type="checkbox" id="agree-check" onChange={({ currentTarget: { checked } }) => { this.setState({ onTermsChange: checked ? true : false }) }} />
                             <span for="agree-check" />
                         </label>
                         <Paragraph className="fs-14 text-white-30 ml-16 mb-0" style={{ flex: 1 }} >
@@ -116,12 +118,12 @@ class WithdrawSummary extends Component {
                         <Translate content="cancel" component={Button} onClick={() => this.onCancel()} type="text" size="large" className="text-white-30 pop-cancel fw-400" />
                     </div>
                 </div>
-                </>
+            </>
         )
     }
 }
 
-const connectStateToProps = ({ sendReceive,userConfig }) => {
+const connectStateToProps = ({ sendReceive, userConfig }) => {
     return { sendReceive, userProfile: userConfig.userProfileInfo }
 }
 const connectDispatchToProps = dispatch => {
