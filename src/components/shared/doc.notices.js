8fb2baab07c3b247fe3acf29f674eba9e3f6c7f7
sowjanya <sowjanya.ziraff>
2021-10-08 21:43:36 +0530

    changes

--- src/components/shared/doc.notices.js
+++ src/components/shared/doc.notices.js
@@ -9,11 +9,11 @@ const DocNotices = (props) => {
     return <React.Fragment style={{ top: 10 }}>
         <Alert style={{ margin: 20, height: 50, padding: 10 }} type="info" message={"Please submit the listed documents to proceed"} showIcon />
         {props?.dashboard?.notices?.data?.map(item => <Alert style={{ margin: 20, cursor: "pointer" }} type="error" showIcon onClick={() => props.history?.push("/documents?id=" + item.typeId)} message={item.title} />)}
-        <Result
+        {props?.dashboard?.notices?.data==null&&<Result
             status="404"
             title={<h4 className="text-white">No request documents, Plese click bellow button to goto dashboard.</h4>}
             extra={<Button className="pop-btn px-36" onClick={() => props.history.push("/dashboard")}>Go to Dashboard</Button>}
-        />
+        />}
     </React.Fragment>
 }
 
