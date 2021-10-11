c2fd255dab0370201ce868851c6946c88b145eae
rao <manoj@zirafftechnologies.com>
2021-10-11 20:46:49 +0530

    doc notices loader

--- src/components/shared/doc.notices.js
+++ src/components/shared/doc.notices.js
@@ -1,19 +1,23 @@
-import { Alert, Result, Button } from 'antd'
+import { Alert, Result, Button, Row, Col, Spin } from 'antd'
 import React, { useEffect } from 'react'
 import { fetchNotices } from '../../reducers/dashboardReducer'
-import connectStateProps from '../../utils/state.connect'
+import connectStateProps from '../../utils/state.connect';
+
 const DocNotices = (props) => {
+
     useEffect(() => {
         props.dispatch(fetchNotices(props.userProfile.id))
     }, [])
     return <React.Fragment style={{ top: 10 }}>
-        <Alert style={{ margin: 20, height: 50, padding: 10 }} type="info" message={"Please submit the listed documents to proceed"} showIcon />
-        {props?.dashboard?.notices?.data?.map(item => <Alert style={{ margin: 20, cursor: "pointer" }} type="error" showIcon onClick={() => props.history?.push("/documents?id=" + item.typeId)} message={item.title} />)}
-        {props?.dashboard?.notices?.data==null&&<Result
-            status="404"
-            title={<h4 className="text-white">No request documents, Plese click bellow button to goto dashboard.</h4>}
-            extra={<Button className="pop-btn px-36" onClick={() => props.history.push("/dashboard")}>Go to Dashboard</Button>}
-        />}
+        <div className="main-container">
+            <Alert style={{ margin: 20, height: 50, padding: 10 }} type="info" message={"Please submit the listed documents to proceed"} showIcon />
+            {props?.dashboard?.notices.loading === true ? <div className="text-center p-24"><Spin size="default" /></div> : <>{props?.dashboard?.notices?.data?.map(item => <Alert style={{ margin: 20, cursor: "pointer" }} type="error" showIcon onClick={() => props.history?.push("/documents?id=" + item.typeId)} message={item.title} />)}</>}
+            {props?.dashboard?.notices?.data == null && <Result
+                status="404"
+                title={<h4 className="text-white">No request documents, Plese click bellow button to goto dashboard.</h4>}
+                extra={<Button className="pop-btn px-36" onClick={() => props.history.push("/dashboard")}>Go to Dashboard</Button>}
+            />}
+        </div>
     </React.Fragment>
 }
 
