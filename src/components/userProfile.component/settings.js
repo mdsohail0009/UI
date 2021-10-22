d196defa231b4f5be87173e50b4103bb0e0b4ead
sowjanya <sowjanya.ziraff>
2021-10-22 21:22:01 +0530

    settings changes

--- src/components/userProfile.component/settings.js
+++ src/components/userProfile.component/settings.js
@@ -1,47 +1,53 @@
-import React, { Component,createRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Typography, Select,Form,Button,message } from 'antd'
 import {getSettingsLuData,saveSettingsData} from '../../api/apiServer'
 import { connect } from 'react-redux';
 import { getmemeberInfo } from '../../reducers/configReduser';
-class Settings extends Component {
-    formRef = createRef();
-    state = {
-        SettingsLu: '',theme:this.props.member?.Theme=='Light Theme'?true:false,settingsObj:{MemberId:'',Language:this.props.member?.Language?this.props.member?.Language:null,LCurrency:this.props.member?.LCurrency?this.props.member?.LCurrency:null,Theme:this.props.member?.Theme?this.props.member?.Theme:null}
-    }
-    componentDidMount(){
-      this.getSettingsLu()
-    }
-    getSettingsLu=async()=>{
+import { useThemeSwitcher } from 'react-css-theme-switcher';
+const Settings =({member,getmemeberInfoa})=> {
+    const { switcher, themes, status } = useThemeSwitcher();
+    const [isDarkMode, setIsDarkMode] = React.useState(true);
+  const [form] = Form.useForm();
+    const [SettingsLu,setSettingsLu]=useState('')
+    const [theme,setTheme]=useState(member?.theme=='Light Theme'?true:false);
+    const [settingsObj,setSettingsObj]=useState({MemberId:'',Language:member?.language?member.language:null,LCurrency:member?.lCurrency?member.lCurrency:null,Theme:member?.theme?member.theme:null})
+    useEffect(()=>{
+      getSettingsLu()
+      switcher({ theme: member?.theme =='Light Theme'? themes.LHT : themes.DRT });
+    },[])
+    const getSettingsLu=async()=>{
         let res = await getSettingsLuData();
         if (res.ok) {
-           this.setState({...this.state,SettingsLu:res.data})
+            setSettingsLu(res.data)
         } 
     }
-    saveSettings=async()=>{
-        let {settingsObj}=this.state;
-        settingsObj.Theme=this.state.theme?'Light Theme':'Dark Theme';
-        settingsObj.MemberId=this.props.member?.id
+    const saveSettings=async()=>{
+        settingsObj.Theme=theme?'Light Theme':'Dark Theme';
+        settingsObj.MemberId=member?.id
         let res = await saveSettingsData(settingsObj);
         if (res.ok) {
             message.destroy()
-            message.error({ content: 'Settings saved successfully' , className:'custom-msg'});
-            this.props.getmemeberInfoa(this.props.member.userId)
+            message.success({ content: 'Settings saved successfully' , className:'custom-msg'});
+            getmemeberInfoa(member.userId)
+            setIsDarkMode(previous => {
+                switcher({ theme: previous ? themes.LHT : themes.DRT });
+                return !previous;
+            });
         } 
     }
-    themeSwitch=() =>{
-        this.setState({...this.state,theme:!this.state.theme})
+    const themeSwitch=() =>{
+        setTheme(!theme)
     }
-    handleChange = (e, fn) => {
-        let { settingsObj } = this.state;
-        settingsObj[fn] = e;
-        this.setState({...this.state,settingsObj:settingsObj})
-        this.formRef.current.setFieldsValue({ ...settingsObj })
+    const handleChange = (e, fn) => {
+        let obj={...settingsObj}
+        obj[fn] = e;
+        setSettingsObj(obj)
+        form.setFieldsValue({settingsObj:obj});
     }
-    render() {
+    // render() {
         const { Option } = Select;
         const { Title, Text, Paragraph } = Typography;
-        const {  settingsObj } = this.state;
-        return (<><Form layout="vertical" initialValues={{ ...settingsObj }} onFinish={() => this.saveSettings()} ref={this.formRef} >
+        return (<><Form layout="vertical" initialValues={{ ...settingsObj }} onFinish={saveSettings} form={form}>
             <div className="box basic-info">
                 <Title className="basicinfo">Settigns</Title>
                 <Paragraph className="basic-decs">User customized settings</Paragraph>
@@ -59,8 +65,8 @@ class Settings extends Component {
                     <Select placeholder="Select Language"  bordered={false}
                      className="cust-input cust-select mb-0"
                      dropdownClassName="select-drpdwn"
-                     onChange={(e) => { this.handleChange(e,'Language') }} value={settingsObj.Language}>
-                        {this.state.SettingsLu.languageLookup?.map((item, idx) => <Option key={idx} value={item}>{item}
+                     onChange={(e) => {settingsObj.Language=e; setSettingsObj(settingsObj);form.setFieldsValue({...settingsObj}) }}>
+                        {SettingsLu.languageLookup?.map((item, idx) => <Option key={idx} value={item}>{item}
                         </Option>)}
                     </Select></Form.Item>
                 </div>
@@ -77,22 +83,22 @@ class Settings extends Component {
                     <Select placeholder="Select Currency"  bordered={false}
                      className="cust-input cust-select mb-0"
                      dropdownClassName="select-drpdwn"
-                     onChange={(e) => { this.handleChange(e,'LCurrency') }} value={settingsObj.LCurrency}>
-                        {this.state.SettingsLu.currencyLookup?.map((item, idx) => <Option key={idx} value={item}>{item}
+                     onChange={(e) => {settingsObj.LCurrency=e; setSettingsObj(settingsObj);form.setFieldsValue({...settingsObj}) }}>
+                        {SettingsLu.currencyLookup?.map((item, idx) => <Option key={idx} value={item}>{item}
                         </Option>)}
                     </Select></Form.Item>
                 </div>
                 <div className="pt-16">
                     <Text className="input-label">Theme</Text>
                     <div className="d-flex">
-                    <div className="theme-switch theme-active" onClick={()=>this.state.theme?this.themeSwitch(true):''}>
+                    <div className="theme-switch theme-active" onClick={()=>theme?themeSwitch(true):''}>
                         <div className="d-flex align-center">
-                            <p className="switch-circle mb-0">{!this.state.theme&&<span className="icon md check-arrow c-pointer"></span>}{this.state.theme&&<span></span>}</p>
+                            <p className="switch-circle mb-0">{!theme&&<span className="icon md check-arrow c-pointer"></span>}{theme&&<span></span>}</p>
                             <p className="mb-0 ml-16 theme-txt">Dark Theme</p></div>
                     </div>
-                    <div className="theme-switch ml-24" onClick={()=>!this.state.theme?this.themeSwitch(false):''}>
+                    <div className="theme-switch ml-24" onClick={()=>!theme?themeSwitch(false):''}>
                         <div className="d-flex align-center">
-                            <p className="switch-circle mb-0">{this.state.theme&&<span className="icon md check-arrow c-pointer"></span>}{!this.state.theme&&<span></span>}</p>
+                            <p className="switch-circle mb-0">{theme&&<span className="icon md check-arrow c-pointer"></span>}{!theme&&<span></span>}</p>
                             <p className="mb-0 ml-16 theme-txt">Light Theme</p></div>
                     </div>
                     </div>
@@ -108,7 +114,7 @@ class Settings extends Component {
             </div>
             </Form>
         </>)
-    }
+    // }
 }
 const connectStateToProps = ({ userConfig }) => {
     return { member: userConfig.userProfileInfo }
