12c3f9f4cada3a2663addbe20fe06e88dc4f7e29
subbareddy <subbareddy@zirafftechnologies.com>
2021-11-01 16:13:52 +0530

    silent renew error

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,6 +1,6 @@
 import { userManager } from "../authentication";
 
-const { USER_FOUND, USER_EXPIRING, processSilentRenew, USER_EXPIRED } = require("redux-oidc");
+const { USER_FOUND, USER_EXPIRING, processSilentRenew, USER_EXPIRED, SILENT_RENEW_ERROR } = require("redux-oidc");
 const USER_LOG_OUT = "userLogout";
 const GET_PROFILE_SUCCESS = "getProfileSuccess";
 const SET_DEVICE_TOKEN = "setDeviceToken";
@@ -15,15 +15,15 @@ const profileSuccess = (info) => {
         payload: info
     }
 }
-const setToken = (payload)=>{
-    return{
-        type:SET_DEVICE_TOKEN,
+const setToken = (payload) => {
+    return {
+        type: SET_DEVICE_TOKEN,
         payload
     }
 }
 let initialState = {
     user: null,
-    deviceToken:null
+    deviceToken: null
 };
 const authReducer = (state = initialState, action) => {
     switch (action.type) {
@@ -32,8 +32,7 @@ const authReducer = (state = initialState, action) => {
             state = { ...state, user: action.payload }
             return state;
         case USER_EXPIRING:
-            debugger
-          userManager.signinSilentCallback().then();
+            userManager.signinSilentCallback().then();
             break;
         case USER_LOG_OUT:
             state = { user: null, profile: null };
@@ -44,9 +43,12 @@ const authReducer = (state = initialState, action) => {
         case GET_PROFILE_SUCCESS:
             state = { ...state, profile: action.payload };
             return state;
-            case SET_DEVICE_TOKEN:
-                state ={...state,deviceToken:action.payload};
-                return state;
+        case SET_DEVICE_TOKEN:
+            state = { ...state, deviceToken: action.payload };
+            return state;
+        case SILENT_RENEW_ERROR:
+            console.log("Silent renew error : "+ action.payload);
+            break;
         default:
             return state;
     }
