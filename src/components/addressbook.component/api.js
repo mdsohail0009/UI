303dd75c88375d3d1ff58109ffde028779f857d6
Naresh <naresh@tlvfintech.com>
2021-11-05 22:51:48 +0530

    adress book call changes

--- src/components/addressbook.component/api.js
+++ src/components/addressbook.component/api.js
@@ -9,21 +9,21 @@ const detailsAddress = (id) => {
     return apiClient.get(ApiControllers.depositWithdraw + `Favourite` );
 }
 const favouriteNameCheck = (member_id, name, type,favaddrId) => {
-    return apiClient.get(ApiControllers.depositWithdraw + `FavouriteNameCheck?memberId=${member_id}&name=${name}&type=${type}&favaddrId=${favaddrId}`);
+    return apiClient.get(ApiControllers.depositWithdraw + `Favourite/${favaddrId}/${member_id}/${name}/${type}`);
 }
 const saveAddress = (obj) => {
-    return apiClient.post(ApiControllers.depositWithdraw + `AddFavourite`, obj);
+    return apiClient.post(ApiControllers.depositWithdraw + `Favourite`, obj);
 }
 const getCoinList = (type) => {
-    return apiClient.get(ApiControllers.buySell+`Coins?type=${type}`);
+    return apiClient.get(ApiControllers.markets + `Coins/${type}`);
 }
 const getSelectedCoinDetails = (coin_code, member_id) => {
     return apiClient.get(ApiControllers.buySell + `${member_id}/Coins/${coin_code}`)
 }
 const activeInactive = (obj) => {
-    return apiClient.post(ApiControllers.master + "useractiveinactive", obj)
+    return apiClient.put(ApiControllers.master + "useractiveinactive", obj)
 }
 const getAddress = (addreddId, type) => {
-    return apiClient.get(ApiControllers.depositWithdraw + `CreateAddressFavourite?addrId=${addreddId}&type=${type}`)
+    return apiClient.get(ApiControllers.depositWithdraw + `Withdraw/${type}/${addreddId}`)
 }
 export {getCoinList,favouriteFiatAddress, saveAddress,getSelectedCoinDetails,detailsAddress,favouriteNameCheck,activeInactive,getAddress}
