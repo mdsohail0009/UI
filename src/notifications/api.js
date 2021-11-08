3b459768f87360f0cfae229a0277d6ff3d88b7ea
subbareddy <subbareddy@zirafftechnologies.com>
2021-11-08 18:53:12 +0530

    notifications

--- src/notifications/api.js
+++ src/notifications/api.js
@@ -4,12 +4,12 @@ const getNotifications = (memberId) => {
     return apiClient.get(ApiControllers.notifications + `${memberId}`)
 }
 const readNotification = (id) => {
-    return apiClient.get(ApiControllers.member + `UpdateReadMsg?id=${id}&isRead=true`)
+    return apiClient.get(ApiControllers.notifications + `UpdateReadMsg?id=${id}&isRead=true`)
 }
 const deleteToken = (obj) => {
-    return apiClient.post(ApiControllers.member + `DeleteUserToken`, obj)
+    return apiClient.post(ApiControllers.accounts + `Tokens`, obj)
 }
 const saveUserToken = (obj) => {
-    return apiClient.post(ApiControllers.member + `SaveUserTokens`, obj)
+    return apiClient.post(ApiControllers.accounts + `Tokens`, obj)
 }
 export { getNotifications, readNotification, deleteToken, saveUserToken }
