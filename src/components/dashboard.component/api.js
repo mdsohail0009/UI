ba99ff9aa2a718f23de6226f9fff8a83eaa7bf6f
rao <rao@tlvfintech.com>
2021-11-10 18:34:29 +0530

    dashboardchart api calls

--- src/components/dashboard.component/api.js
+++ src/components/dashboard.component/api.js
@@ -24,24 +24,4 @@ const getPortfolioGraph = (memId, type) => {
 const getCoinChatData = (coinName,currency, days) => {
     return coinGekoClient.get(`coins/${coinName}/market_chart?vs_currency=${currency}&days=${days}`);
 }
-
-const getdshKpis = (userid) => {
-    return apiClient.get(ApiControllers.dashboard + `KPI/${userid}`);
-}
-const getdshcumulativePnl = (userid,days) => {
-    return apiClient.get(ApiControllers.dashboard + `CumulativePNL/${userid}/${days}`);
-}
-const getAssetNetwroth = (userid,days) => {
-    return apiClient.get(ApiControllers.dashboard + `AssetsNetWorth/${userid}/${days}`);
-}
-const getAssetAllowcation = (userid,days) => {
-    return apiClient.get(ApiControllers.dashboard + `AssetAllocation/${userid}/${days}`);
-}
-const getprofits = (userid,days) => {
-    return apiClient.get(ApiControllers.dashboard + `Profits/${userid}/${days}`);
-}
-const getdailypnl = (userid,days) => {
-    return apiClient.get(ApiControllers.dashboard + `DailyPNL/${userid}/${days}`);
-}
-export { fetchMarketCaps, fetchMemberWallets, fetchPortfolio, fetchYourPortfolio, getNotices, getPortfolioGraph, getcoinDetails, getCoinChatData, 
-    getdshKpis,getdshcumulativePnl,getAssetNetwroth,getAssetAllowcation,getprofits,getdailypnl}
+export { fetchMarketCaps, fetchMemberWallets, fetchPortfolio, fetchYourPortfolio, getNotices, getPortfolioGraph, getcoinDetails, getCoinChatData}
