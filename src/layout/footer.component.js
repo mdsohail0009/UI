4cc20b697da1b1972c401dfc226db20a646fd8a2
Manoj kumar <manoj.ziraff@gmail.com>
2021-11-13 16:18:19 +0530

    doc notices and footer responsive

--- src/layout/footer.component.js
+++ src/layout/footer.component.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Layout, Typography, Button } from 'antd';
 import counterpart from 'counterpart';
 import en from '../lang/en';
@@ -14,50 +14,35 @@ counterpart.registerTranslations('ch', ch);
 counterpart.registerTranslations('my', my);
 
 const { Footer: AntFooter } = Layout
+const { Text } = Typography;
 
-
-function Footer({member}) {
+function Footer({ member }) {
     const { switcher, themes, status } = useThemeSwitcher();
     const [theme, setTheme] = useState('LRT');
 
 
-    useEffect(()=>{
-        switcher({ theme: member?.theme =='Light Theme'? themes.LHT : themes.DRT });
-        counterpart.setLocale(member?.language?member?.language:'en');
-      },[])
+    useEffect(() => {
+        switcher({ theme: member?.theme == 'Light Theme' ? themes.LHT : themes.DRT });
+        counterpart.setLocale(member?.language ? member?.language : 'en');
+    }, [])
     if (status == 'loading') {
         return <div>Loading styles...</div>;
     }
 
-   
-   
+
+
 
 
     return (<AntFooter style={{ backgroundColor: 'transparent', padding: 0 }}>
-
-
-        <div className="main-container footer-links">
-            <div className="d-flex justify-content align-center">
-                
+        <div className="main-container">
+            <div className=" footer-links">
                 {/* <a href="https://suissebase.ch/" target="_blank">Home</a> */}
-                <a href="https://suissebase.ch/" target="_blank"><Translate content="home"/></a>
+                <a href="https://suissebase.ch/" target="_blank"><Translate content="home" /></a>
                 {/* <a href="https://suissebase.ch/" target="_blank">Careers</a> */}
-                <a href="https://suissebase.ch/" target="_blank"><Translate content="careers"/></a>
+                <a href="https://suissebase.ch/" target="_blank"><Translate content="careers" /></a>
                 {/* <a href="https://www.iubenda.com/privacy-policy/42856099" target="_blank">Legal & Policy</a> */}
-                <a href="https://www.iubenda.com/privacy-policy/42856099" target="_blank"><Translate content="legal_policy"/></a>
-                <Typography className="mobile-none text-white-30">Suissebase<sup className="fs-10">TM</sup> {new Date().getFullYear()}</Typography>
-            </div>
-            <div className="copyright-block">
-                <Typography className="copyright text-white-30">Suissebase<sup className="fs-10">TM</sup> {new Date().getFullYear()}</Typography>
-                <span>
-                    {/* <Button type="primary" shape="circle" size="large" className='darkTheme' onClick={handleTheme}> {theme}</Button> */}
-                    
-                    {/* <select value={lang} onChange={onLangChange} className="selct-lang ml-8 f-12" removeIcon={true}>
-                        <option value="en">EN</option>
-                        <option value="ch">英语</option>
-                        <option value="my">MY</option>
-                    </select> */}
-                </span>
+                {/* <a href="https://www.iubenda.com/privacy-policy/42856099" target="_blank"><Translate content="legal_policy" /></a> */}
+                <Text className="text-white-30">Suissebase<sup className="fs-10">TM</sup> {new Date().getFullYear()}</Text>
             </div>
         </div>
     </AntFooter>)
