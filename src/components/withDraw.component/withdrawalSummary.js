402e877873d3664ef234b0c13a75043cadc73b37
rao@tlvfintech.com <manoj.ziraff@gmail.com>
2021-11-22 17:07:16 +0530

    verification code design

--- src/components/withDraw.component/withdrawalSummary.js
+++ src/components/withDraw.component/withdrawalSummary.js
@@ -1,58 +1,63 @@
-import React, { useState} from 'react';
-import { Typography, Button,Form,Input,Row, Col,Search } from 'antd';
+import React, { useState } from 'react';
+import { Typography, Button, Form, Input, Row, Col, Search } from 'antd';
 import Currency from '../shared/number.formate';
 import { setStep } from '../../reducers/buysellReducer';
 import { connect } from 'react-redux';
 import Translate from 'react-translate-component';
 
 
-const WithdrawalSummary = ({ sendReceive, onConfirm,onCancel }) => {
+const WithdrawalSummary = ({ sendReceive, onConfirm, onCancel }) => {
   const { Text } = Typography;
   const [isLoding, setIsLoding] = useState(false);
-  const saveWithdrwal = async() =>{
+  const saveWithdrwal = async () => {
     setIsLoding(true)
     onConfirm()
   }
-    return (
-        <div className="mt-16">
-        <Text className="fs-14 text-white-50 fw-200"> <Translate content="amount" component={Text} className="fs-14 text-white-50 fw-200" /></Text>
-        <Currency className="fs-20 text-white-30 mb-36" prefix={""} defaultValue={sendReceive.withdrawFiatObj?.totalValue} suffixText={sendReceive.withdrawFiatObj?.walletCode} />
-        <Text className="fs-14 text-white-50 fw-200"> <Translate content="Bank_account" component={Text} className="fs-14 text-white-50 fw-200"   /></Text>
-        <Text className="fs-20 text-white-30 d-block mb-36">{sendReceive.withdrawFiatObj?.accountNumber}</Text>
-        <Text className="fs-14 text-white-50 fw-200"><Translate content="BIC_SWIFT_routing_number" component={Text} className="fs-14 text-white-50 fw-200"    /></Text>
-        <Text className="fs-20 text-white-30 d-block mb-36">{sendReceive.withdrawFiatObj?.routingNumber}</Text>
-        <Text className="fs-14 text-white-50 fw-200"><Translate content="Bank_name" component={Text} className="fs-14 text-white-50 fw-200"   /></Text>
-        <Text className="fs-20 text-white-30 d-block mb-36">{sendReceive.withdrawFiatObj?.bankName}</Text>
-        <Text className="fs-14 text-white-50 fw-200"><Translate content="Recipient_full_name" component={Text} className="fs-14 text-white-50 fw-200"   /></Text>
-        <Text className="fs-20 text-white-30 d-block mb-36">{sendReceive.withdrawFiatObj?.beneficiaryAccountName}</Text>       
-        <Form.Item label="Phone Verification Code" extra="Enter 6 digit code sent to ">
-                        <Input.Group>
-                        <Input style={{ width: 'calc(100% - 100px)' }}  />
-                       <Button type="primary">Get Code</Button>
-                        </Input.Group>
-                    </Form.Item>
-        <ul className="pl-0 ml-16 text-white-50 mb-24">
-          <li> <Translate className="pl-0 ml-16 text-white-50" content="account_details" component={Text}/> </li>
-          <li><Translate className="pl-0 ml-16 text-white-50" content="Cancel_select" component={Text}/></li>
-        </ul>
+  return (
+    <div className="mt-16">
+      <Text className="fs-14 text-white-50 fw-200"> <Translate content="amount" component={Text} className="fs-14 text-white-50 fw-200" /></Text>
+      <Currency className="fs-20 text-white-30 mb-36" prefix={""} defaultValue={sendReceive.withdrawFiatObj?.totalValue} suffixText={sendReceive.withdrawFiatObj?.walletCode} />
+      <Text className="fs-14 text-white-50 fw-200"> <Translate content="Bank_account" component={Text} className="fs-14 text-white-50 fw-200" /></Text>
+      <Text className="fs-20 text-white-30 d-block mb-36">{sendReceive.withdrawFiatObj?.accountNumber}</Text>
+      <Text className="fs-14 text-white-50 fw-200"><Translate content="BIC_SWIFT_routing_number" component={Text} className="fs-14 text-white-50 fw-200" /></Text>
+      <Text className="fs-20 text-white-30 d-block mb-36">{sendReceive.withdrawFiatObj?.routingNumber}</Text>
+      <Text className="fs-14 text-white-50 fw-200"><Translate content="Bank_name" component={Text} className="fs-14 text-white-50 fw-200" /></Text>
+      <Text className="fs-20 text-white-30 d-block mb-36">{sendReceive.withdrawFiatObj?.bankName}</Text>
+      <Text className="fs-14 text-white-50 fw-200"><Translate content="Recipient_full_name" component={Text} className="fs-14 text-white-50 fw-200" /></Text>
+      <Text className="fs-20 text-white-30 d-block mb-36">{sendReceive.withdrawFiatObj?.beneficiaryAccountName}</Text>
+      <ul className="pl-0 ml-16 text-white-50 my-36">
+        <li><Translate className="pl-0 ml-0 text-white-50" content="account_details" component={Text} /> </li>
+        <li><Translate className="pl-0 ml-0 text-white-50" content="Cancel_select" component={Text} /></li>
+      </ul>
+      <Form className="mt-36">
+        <Form.Item
+          name="benficiaryBankName"
+          className="input-label otp-verify my-36"
+          extra={<Text className="fs-12 text-white-30 fw-200">Enter 6 digit code sent to *******1254</Text>}
+        >
+          <Input className="cust-input text-left" placeholder="Enter Verification Code" maxLength={8} />
+          <Button type="text">GET CODE</Button>
+          {/* <Button type="text">RESEND CODE</Button> */}
+        </Form.Item>
         <Button
-        disabled={isLoding}
-              size="large"
-              block
-              className="pop-btn"
-              onClick={saveWithdrwal}
-            >
-             <Translate content="Confirm" component={Text}/>
-             </Button>
-            <div className="text-center">
-        <Translate content="back" component={Button} onClick={() => onCancel()} type="text" size="large" className="text-center text-white-30 pop-cancel fw-400 fs-14 text-upper text-center"/>
+          disabled={isLoding}
+          size="large"
+          block
+          className="pop-btn"
+          onClick={saveWithdrwal}
+        >
+          <Translate content="Confirm" component={Text} />
+        </Button>
+      </Form>
+      <div className="text-center">
+        <Translate content="back" component={Button} onClick={() => onCancel()} type="text" size="large" className="text-center text-white-30 pop-cancel fw-400 fs-14 text-upper text-center" />
       </div>
-      </div>
-    )
+    </div>
+  )
 }
 
 const connectStateToProps = ({ userConfig, sendReceive }) => {
-  return { userConfig: userConfig.userProfileInfo,sendReceive }
+  return { userConfig: userConfig.userProfileInfo, sendReceive }
 }
 const connectDispatchToProps = dispatch => {
   return {
