f843244c54fd2222fe9ec73ecd1cea9e50d6ed12
rao <rao@tlvfintech.com>
2021-12-07 20:35:33 +0530

    encryption of Notification call

--- src/notifications/api.js
+++ src/notifications/api.js
@@ -3,8 +3,8 @@ import { ApiControllers } from '../api/config';
 const getNotifications = (memberId) => {
     return apiClient.get(ApiControllers.notifications + `${memberId}`)
 }
-const readNotification = (id) => {
-    return apiClient.get(ApiControllers.notifications + `UpdateReadMsg?id=${id}&isRead=true`)
+const readNotification = (id,isRead) => {
+    return apiClient.put(ApiControllers.notifications + `UpdateReadMsg/${id}/${isRead}`)
 }
 const deleteToken = (obj) => {
     return apiClient.post(ApiControllers.accounts + `Tokens`, obj)
