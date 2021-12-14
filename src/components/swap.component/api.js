db977fa23bd7842c79ddc540f0351a703d0a2519
“Naresh” <naresh@tlvfintech.com>
2021-12-14 22:24:31 +0530

    swap COMMISION CALL ADDED

--- src/components/swap.component/api.js
+++ src/components/swap.component/api.js
@@ -7,6 +7,9 @@ const getportfolio = (member_id) => {
 const fetchCurrConvertionValue = (from, to, value, memeberid, screenName) => {
     return apiClient.get(ApiControllers.master + `CryptoToCrypto/${memeberid}/${from}/${to}/${value}/${screenName || null}`);
 }
+const fetchCurrConvertionCommisionValue = (from, to, value, memeberid, screenName) => {
+    return apiClient.get(ApiControllers.master + `Comission/${memeberid}/${from}/${to}/${value}/${screenName || null}`);
+}
 const swapPreviewData = (coin, currency, amount) => {
     return apiClient.get(ApiControllers.swap + `Preview?coin=${coin}&currency=${currency}&amount=${amount}`);
 }
@@ -19,4 +22,4 @@ const gettoCoinList = (member_id, fromcoin) => {
 const saveSwapData = (obj) => {
     return apiClient.post(ApiControllers.swap + 'Swap', obj);
 }
-export { getportfolio, fetchCurrConvertionValue, swapPreviewData, saveSwapData, getfromCoinList, gettoCoinList }
+export { getportfolio, fetchCurrConvertionValue, swapPreviewData, saveSwapData, getfromCoinList, gettoCoinList,fetchCurrConvertionCommisionValue }
