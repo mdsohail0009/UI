964a49942c5f4b403c3a8b10700b2340fb91e011
rao <manoj@zirafftechnologies.com>
2022-01-05 20:06:28 +0530

    welcome text

--- src/components/home.component/index.js
+++ src/components/home.component/index.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import { Layout, Menu, Modal, Typography, Dropdown, Row, Col, Divider, Avatar, Carousel, Switch, Drawer, Button } from 'antd';
 
 class HomeComponent extends Component {
     render() {
+        const { Title, Text, Paragraph } = Typography;
         return (
-            <div>
-                Home
-            </div>
+            <>
+                <div className="text-center">
+                    <Text className="d-block wlecome-text">Welcome John Doe</Text>
+                    <Text>Good Morning</Text>
+
+                </div>
+
+            </>
         )
     }
 }
