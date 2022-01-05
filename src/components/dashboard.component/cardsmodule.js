f0c630e5f4bb3ceb24c25033f70ba94f12df5c1b
rao <rao@tlvfintech.com>
2022-01-05 10:56:35 +0530

    card module changes

--- src/components/dashboard.component/cardsmodule.js
+++ src/components/dashboard.component/cardsmodule.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
-import { Card, Row, Col, Typography, Tooltip,Tabs,List,Avatar   } from 'antd';
+import { Card, Row, Col, Typography, Tooltip,Tabs,List,Avatar,Image } from 'antd';
 import apiCalls from "../../api/apiCalls";
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+// import logoWhite from '../assets/images/logo-white.png';
 import { AppleOutlined, AndroidOutlined } from '@ant-design/icons';
 const { Title, Paragraph, Text } = Typography;
 const { TabPane } = Tabs;
@@ -24,7 +25,7 @@ render() {
     <div>
     <Tabs defaultActiveKey="1" centered className='text-white card-list'>
         <TabPane tab={<span className='tab-block'> <span className="coin lg ETH mb-16 mx-auto"></span> Overview</span>} key="1" > 
-            <div className='wallet-total p-24 mt-36'>
+            <div className='wallet-total mx-auto p-24 my-30'>
                 <List 
                 itemLayout="horizontal"
                 dataSource={data}
@@ -39,9 +40,292 @@ render() {
                 )}
             />
             </div>
+            <div className='text-left'>
+                <div className='pt-16 mt-36 mb-28'>
+                    <Title className='fs-22 fw-600 mb-8 text-white-30'>Know Your Card</Title>
+                    <Paragraph className='text-white-50 fs-16 mb-0 l-height-normal'>Select card to check spends and avilable limit</Paragraph>
+                </div>
+                <div className="site-card-wrapper">
+                    <Row gutter={24}>
+                    <Col span={8}>
+                        <Card  className="card-module crypto-card mb-16 c-pointer" bordered={false} >
+                            <Row gutter={24}>
+                                <Col span={12}>
+                                    <div className="align-center">
+                                        <Paragraph className="fs-22 fw-600 text-purewhite ml-8 mb-0">272.48 USD</Paragraph>
+                                        <Paragraph className="fs-18 fw-500 text-purewhite ml-8 mb-0">Avilable</Paragraph>
+                                    </div>
+                                    </Col>
+                                <Col span={12}>
+                                    <div className="align-center">
+                                        <Paragraph className="fs-22 fw-600 text-purewhite ml-8 mb-0">177.52 USD</Paragraph>
+                                        <Paragraph className="fs-18 fw-500 text-purewhite ml-8 mb-0">Spent</Paragraph>
+                                    </div>
+                                </Col>
+                            </Row>
+                        <div className="text-white">
+                            <div className="fs-28 fw-400">XXXX  XXXX  XXXX  4578</div>
+                            <div className='text-right valid-time'>Valid Upto 02/23</div>
+                        </div>
+                        <div className="text-white cardholder-name fw-600">
+                            Suisse - Jhon Doe
+                        </div>
+                    </Card>
+                    </Col>
+                    <Col span={8}>
+                        <Card  className="card-module crypto-card mb-16 c-pointer" bordered={false} >
+                            <Row gutter={24}>
+                                <Col span={12}>
+                                    <div className="align-center">
+                                        <Paragraph className="fs-22 fw-600 text-purewhite ml-8 mb-0">400.18 USD</Paragraph>
+                                        <Paragraph className="fs-18 fw-500 text-purewhite ml-8 mb-0">Avilable</Paragraph>
+                                    </div>
+                                    </Col>
+                                <Col span={12}>
+                                    <div className="align-center">
+                                        <Paragraph className="fs-22 fw-600 text-purewhite ml-8 mb-0">100.82 USD</Paragraph>
+                                        <Paragraph className="fs-18 fw-500 text-purewhite ml-8 mb-0">Spent</Paragraph>
+                                    </div>
+                                </Col>
+                            </Row>
+                        <div className="text-white">
+                            <div className="fs-28 fw-400">XXXX  XXXX  XXXX  2693</div>
+                            <div className='text-right valid-time'>Valid Upto 06/23</div>
+                        </div>
+                        <div className="text-white cardholder-name fw-600">
+                            Suisse - User Name
+                        </div>
+                    </Card>
+                    </Col>
+                    <Col span={8}>
+                        <Card  className="virtual-card crypto-card mb-16 c-pointer" bordered={false} >
+                            <Row gutter={24}>
+                                <Col span={12}>
+                                    <div className="align-center">
+                                        <Paragraph className="fs-18 fw-500 text-purewhite ml-8 mb-0">Avilable</Paragraph>
+                                    </div>
+                                    </Col>
+                                <Col span={12}>
+                                    <div className="text-right">
+                                    <Image width={75} src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg" />
+                                    </div>
+                                </Col>
+                            </Row>
+                            <div className="text-white">
+                            <span className="coin lg ETH"></span>
+                            <div className="fs-28 fw-400">XXXX  XXXX  XXXX  2693</div>
+                        </div>
+                        </Card>
+                    </Col>
+                     
+                    </Row>
+                </div>
+            </div>
         </TabPane>
-        <TabPane tab={<span className='tab-block'> <span className="coin lg ETH mb-16 mx-auto"></span> Physical Cards</span>}key="2"> Tab 2</TabPane>
-        <TabPane tab={<span className='tab-block'> <span className="coin lg ETH mb-16 mx-auto"></span> Virtual Cards</span>} key="3" > Tab 1 </TabPane>
+        <TabPane tab={<span className='tab-block'> <span className="coin lg ETH mb-16 mx-auto"></span> Physical Cards</span>}key="2"> 
+            <div className="site-card-wrapper text-left my-30">
+                <Row gutter={24} className='my-16'>
+                    <Col span={8}>
+                        <Card  className="virtual-card crypto-card mb-16 c-pointer" bordered={false} >
+                                <Row gutter={24}>
+                                    <Col span={12}>
+                                        <div className="align-center">
+                                            <Paragraph className="fs-18 fw-500 text-purewhite ml-8 mb-0">Avilable</Paragraph>
+                                        </div>
+                                        </Col>
+                                    <Col span={12}>
+                                        <div className="text-right">
+                                        <Image width={75} src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg" />
+                                        </div>
+                                    </Col>
+                                </Row>
+                                <div className="text-white">
+                                <span className="coin lg ETH"></span>
+                                <div className="fs-28 fw-400">XXXX  XXXX  XXXX  2693</div>
+                            </div>
+                        </Card>
+                    </Col>
+                    <Col span={8}>
+                        <Card  className="virtual-card crypto-card mb-16 c-pointer" bordered={false} >
+                                <Row gutter={24}>
+                                    <Col span={12}>
+                                        <div className="align-center">
+                                            <Paragraph className="fs-18 fw-500 text-purewhite ml-8 mb-0">Avilable</Paragraph>
+                                        </div>
+                                        </Col>
+                                    <Col span={12}>
+                                        <div className="text-right">
+                                        <Image width={75} src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg" />
+                                        </div>
+                                    </Col>
+                                </Row>
+                                <div className="text-white">
+                                <span className="coin lg ETH"></span>
+                                <div className="fs-28 fw-400">XXXX  XXXX  XXXX  2693</div>
+                            </div>
+                        </Card>
+                    </Col>
+                </Row>
+            </div>
+        </TabPane>
+        <TabPane tab={<span className='tab-block'> <span className="coin lg ETH mb-16 mx-auto"></span> Virtual Cards</span>} key="3" > 
+            <div className="site-card-wrapper text-left my-30">
+                <Row gutter={24} className='my-16'>
+                    <Col span={8}>
+                        <Card  className="card-module crypto-card mb-16 c-pointer" bordered={false} >
+                            <Row gutter={24}>
+                                <Col span={12}>
+                                    <div className="align-center">
+                                        <Paragraph className="fs-22 fw-600 text-purewhite ml-8 mb-0">272.48 USD</Paragraph>
+                                        <Paragraph className="fs-18 fw-500 text-purewhite ml-8 mb-0">Avilable</Paragraph>
+                                    </div>
+                                    </Col>
+                                <Col span={12}>
+                                    <div className="align-center">
+                                        <Paragraph className="fs-22 fw-600 text-purewhite ml-8 mb-0">177.52 USD</Paragraph>
+                                        <Paragraph className="fs-18 fw-500 text-purewhite ml-8 mb-0">Spent</Paragraph>
+                                    </div>
+                                </Col>
+                            </Row>
+                        <div className="text-white">
+                            <div className="fs-28 fw-400">XXXX  XXXX  XXXX  4578</div>
+                            <div className='text-right valid-time'>Valid Upto 02/23</div>
+                        </div>
+                        <div className="text-white cardholder-name fw-600">
+                            Suisse - Jhon Doe
+                        </div>
+                    </Card>
+                    </Col>
+                    <Col span={8}>
+                        <Card  className="card-module crypto-card mb-16 c-pointer" bordered={false} >
+                            <Row gutter={24}>
+                                <Col span={12}>
+                                    <div className="align-center">
+                                        <Paragraph className="fs-22 fw-600 text-purewhite ml-8 mb-0">400.18 USD</Paragraph>
+                                        <Paragraph className="fs-18 fw-500 text-purewhite ml-8 mb-0">Avilable</Paragraph>
+                                    </div>
+                                    </Col>
+                                <Col span={12}>
+                                    <div className="align-center">
+                                        <Paragraph className="fs-22 fw-600 text-purewhite ml-8 mb-0">100.82 USD</Paragraph>
+                                        <Paragraph className="fs-18 fw-500 text-purewhite ml-8 mb-0">Spent</Paragraph>
+                                    </div>
+                                </Col>
+                            </Row>
+                        <div className="text-white">
+                            <div className="fs-28 fw-400">XXXX  XXXX  XXXX  2693</div>
+                            <div className='text-right valid-time'>Valid Upto 06/23</div>
+                        </div>
+                        <div className="text-white cardholder-name fw-600">
+                            Suisse - User Name
+                        </div>
+                    </Card>
+                    </Col>
+                    <Col span={8}>
+                        <Card  className="card-module crypto-card mb-16 c-pointer" bordered={false} >
+                            <Row gutter={24}>
+                                <Col span={12}>
+                                    <div className="align-center">
+                                        <Paragraph className="fs-22 fw-600 text-purewhite ml-8 mb-0">400.18 USD</Paragraph>
+                                        <Paragraph className="fs-18 fw-500 text-purewhite ml-8 mb-0">Avilable</Paragraph>
+                                    </div>
+                                    </Col>
+                                <Col span={12}>
+                                    <div className="align-center">
+                                        <Paragraph className="fs-22 fw-600 text-purewhite ml-8 mb-0">100.82 USD</Paragraph>
+                                        <Paragraph className="fs-18 fw-500 text-purewhite ml-8 mb-0">Spent</Paragraph>
+                                    </div>
+                                </Col>
+                            </Row>
+                        <div className="text-white">
+                            <div className="fs-28 fw-400">XXXX  XXXX  XXXX  2693</div>
+                            <div className='text-right valid-time'>Valid Upto 06/23</div>
+                        </div>
+                        <div className="text-white cardholder-name fw-600">
+                            Suisse - User Name
+                        </div>
+                    </Card>
+                    </Col>
+                </Row>
+                <Row gutter={24} className='my-16'>
+                    <Col span={8}>
+                        <Card  className="card-module crypto-card mb-16 c-pointer" bordered={false} >
+                            <Row gutter={24}>
+                                <Col span={12}>
+                                    <div className="align-center">
+                                        <Paragraph className="fs-22 fw-600 text-purewhite ml-8 mb-0">272.48 USD</Paragraph>
+                                        <Paragraph className="fs-18 fw-500 text-purewhite ml-8 mb-0">Avilable</Paragraph>
+                                    </div>
+                                    </Col>
+                                <Col span={12}>
+                                    <div className="align-center">
+                                        <Paragraph className="fs-22 fw-600 text-purewhite ml-8 mb-0">177.52 USD</Paragraph>
+                                        <Paragraph className="fs-18 fw-500 text-purewhite ml-8 mb-0">Spent</Paragraph>
+                                    </div>
+                                </Col>
+                            </Row>
+                        <div className="text-white">
+                            <div className="fs-28 fw-400">XXXX  XXXX  XXXX  4578</div>
+                            <div className='text-right valid-time'>Valid Upto 02/23</div>
+                        </div>
+                        <div className="text-white cardholder-name fw-600">
+                            Suisse - Jhon Doe
+                        </div>
+                    </Card>
+                    </Col>
+                    <Col span={8}>
+                        <Card  className="card-module crypto-card mb-16 c-pointer" bordered={false} >
+                            <Row gutter={24}>
+                                <Col span={12}>
+                                    <div className="align-center">
+                                        <Paragraph className="fs-22 fw-600 text-purewhite ml-8 mb-0">400.18 USD</Paragraph>
+                                        <Paragraph className="fs-18 fw-500 text-purewhite ml-8 mb-0">Avilable</Paragraph>
+                                    </div>
+                                    </Col>
+                                <Col span={12}>
+                                    <div className="align-center">
+                                        <Paragraph className="fs-22 fw-600 text-purewhite ml-8 mb-0">100.82 USD</Paragraph>
+                                        <Paragraph className="fs-18 fw-500 text-purewhite ml-8 mb-0">Spent</Paragraph>
+                                    </div>
+                                </Col>
+                            </Row>
+                        <div className="text-white">
+                            <div className="fs-28 fw-400">XXXX  XXXX  XXXX  2693</div>
+                            <div className='text-right valid-time'>Valid Upto 06/23</div>
+                        </div>
+                        <div className="text-white cardholder-name fw-600">
+                            Suisse - User Name
+                        </div>
+                    </Card>
+                    </Col>
+                    <Col span={8}>
+                        <Card  className="card-module crypto-card mb-16 c-pointer" bordered={false} >
+                            <Row gutter={24}>
+                                <Col span={12}>
+                                    <div className="align-center">
+                                        <Paragraph className="fs-22 fw-600 text-purewhite ml-8 mb-0">400.18 USD</Paragraph>
+                                        <Paragraph className="fs-18 fw-500 text-purewhite ml-8 mb-0">Avilable</Paragraph>
+                                    </div>
+                                    </Col>
+                                <Col span={12}>
+                                    <div className="align-center">
+                                        <Paragraph className="fs-22 fw-600 text-purewhite ml-8 mb-0">100.82 USD</Paragraph>
+                                        <Paragraph className="fs-18 fw-500 text-purewhite ml-8 mb-0">Spent</Paragraph>
+                                    </div>
+                                </Col>
+                            </Row>
+                        <div className="text-white">
+                            <div className="fs-28 fw-400">XXXX  XXXX  XXXX  2693</div>
+                            <div className='text-right valid-time'>Valid Upto 06/23</div>
+                        </div>
+                        <div className="text-white cardholder-name fw-600">
+                            Suisse - User Name
+                        </div>
+                    </Card>
+                    </Col>
+                </Row>
+            </div>
+        </TabPane>
         <TabPane tab={<span className='tab-block'> <span className="coin lg ETH mb-16 mx-auto"></span> Settings</span>}key="4"> Tab 2</TabPane>
     </Tabs>
     </div>
