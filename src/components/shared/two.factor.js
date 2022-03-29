f38607043504fc6845fd66f1a2b6112dfc794851
rao <manoj@zirafftechnologies.com>
2022-03-29 20:02:38 +0530

    t2fa

--- src/components/shared/two.factor.js
+++ src/components/shared/two.factor.js
@@ -6,8 +6,8 @@ const TwoFactor = (props) => {
     return <React.Fragment style={{ top: 10 }}>
         <div className="main-container">
             < Result status="404"
-                title={<h4 className="text-white">Please click on bellow button to go Security for enable 2FA.</h4>}
-                extra={<Button className="pop-btn px-36" onClick={() => { props.history.push("/userprofile") }}>Go to Security</Button>}
+                title={<h4 className="text-white">Please enable two-factor authentication (2FA) in Security section. Please click on bellow button to go Security for enable 2FA.</h4>}
+                extra={<Button className="pop-btn px-36" onClick={() => { props.history.push("/userprofile?key=2") }}>Go to Security</Button>}
             />
         </div>
     </React.Fragment>
