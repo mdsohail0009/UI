e3e55894fb4bf7711c96e56c1c0b855a033c63c5
rao <manoj@zirafftechnologies.com>
2022-04-01 22:42:25 +0530

    country lookup

--- src/components/addressbook.component/addFiatAddressbook.js
+++ src/components/addressbook.component/addFiatAddressbook.js
@@ -65,8 +65,8 @@ const NewFiatAddress = ({ buyInfo, userConfig, onCancel, addressBookReducer, use
             loadDataAddress();
         }
         addressbkTrack();
-        // getCountryLu();
-        // getStateLu();
+        getCountryLu();
+        getStateLu();
     }, [])
     const addressbkTrack = () => {
         apiCalls.trackEvent({ "Type": 'User', "Action": 'Withdraw Fiat Address Book Details page view ', "Username": userProfileInfo?.userName, "MemeberId": userProfileInfo?.id, "Feature": 'Withdraw Fiat', "Remarks": 'Withdraw Fiat Address book details view', "Duration": 1, "Url": window.location.href, "FullFeatureName": 'Withdraw Fiat' });
