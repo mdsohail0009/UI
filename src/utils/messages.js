0abb1c73a10a9ff124d1c088d23bd27a18a46848
rao <manoj@zirafftechnologies.com>
2022-04-08 13:50:35 +0530

    messaages

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,53 +1,27 @@
 import { message } from "antd";
 
-
-
 export const success = (msg) => {
-
     message.destroy();
-
     message.success({
-
         content: msg,
-
         className: "custom-msg",
-
         duration: 1
-
     });
-
 };
-
 export const warning = (msg) => {
-
     message.destroy();
-
     message.warning({
-
         content: msg,
-
         className: "custom-msg",
-
         duration: 0.75
-
     });
-
 };
-
-
 
 export const error = (msg) => {
-
     message.destroy();
-
     message.error({
-
         content: msg,
-
         className: 'custom-msg',
-
         duration: 0.5
-
       });
-
 };
