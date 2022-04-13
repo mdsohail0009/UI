11371009e054ab3e4e82fa95a0bae7692565c958
rao <manoj@zirafftechnologies.com>
2022-04-13 18:11:10 +0530

    notices

--- src/components/dashboard.component/notices.js
+++ src/components/dashboard.component/notices.js
@@ -1,37 +1,32 @@
-import { Alert, Carousel, Typography, Button } from "antd";
+import { Alert, Carousel } from "antd";
 import { useEffect, useState } from "react"
-import Translate from "react-translate-component";
 import Loader from "../../Shared/loader";
 import connectStateProps from "../../utils/state.connect";
 import { getNotices } from "./api";
-const { Title } = Typography;
-const Notices = ({ userProfile }) => {
+
+const Notices = () => {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState({ hasError: false, message: null });
     const [notices, setNotices] = useState([]);
     useEffect(() => { fetchNotices(); }, []);
     const fetchNotices = async () => {
-        const response = await getNotices(userProfile.id);
+        const response = await getNotices();
         if (response.ok) {
             setNotices(response.data);
         } else {
-            setError({ hasError: true, message: response.data });
+            setError({ hasError: true, message: response.originalError });
         }
         setLoading(false)
     }
     if (loading) {
         return <Loader />
     }
-    if (!error.hasError) {
+    if (error.hasError) {
         return <Alert type="error" message="Alert" description="Something went wrong please try again!" showIcon />
     }
-    return <Carousel autoplay className="mb-24">
-        {notices.map((notice, indx) => <div className="p-28 carousel-card">
-            <Title className="fs-24 text-black mb-4" title={notice.title} />
-            <div className="fs-16 text-black mb-24"  >
-                {notice.htmlContent}
-            </div>
-            {/* <Translate content="db_slider_btn" component={Button} type="primary" className="custom-btn fs-14 prime mb-24" /> */}
+    return <Carousel autoplay className="mb-16">
+        {notices.map((notice, indx) => <div key={indx} className="p-28 carousel-card">
+            <div className="fs-16 text-black mb-24" dangerouslySetInnerHTML={{__html: notice.notice}}></div>
         </div>)}
     </Carousel>
 }
