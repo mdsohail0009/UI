1e9a168d213bfda56315b630fb0d927b24d6b412
Swapna <swapna@zirafftechnologies.com>
2022-05-04 16:22:31 +0530

    text changes

--- src/components/withdraw.crypto.component/withdraw.selected.wallet.js
+++ src/components/withdraw.crypto.component/withdraw.selected.wallet.js
@@ -243,7 +243,7 @@ class CryptoWithDrawWallet extends Component {
                             name="toWalletAddress"
                             className="custom-forminput custom-label  mb-16"
                             required
-                            label={apicalls.convertLocalLang('address')}
+                            label={apicalls.convertLocalLang('sendTo')}
                         >
                             <div className="p-relative d-flex align-center">
                                 {/* <Input className="cust-input custom-add-select mb-0" placeholder="Enter address" value={this.state.walletAddress} */}
