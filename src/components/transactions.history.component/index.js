f2dcdf16718dc26899594b1d52c17de07ec76ab2
rao <rao@tlvfintech.com>
2022-05-06 20:26:34 +0000

    Updated index.js

--- src/components/transactions.history.component/index.js
+++ src/components/transactions.history.component/index.js
@@ -1,8 +1,19 @@
-import React, { Component } from 'react';
-import { Drawer, Tabs, Collapse, Typography ,Row, Col, Select, Button, Form} from 'antd';
-import { connect } from 'react-redux';
-import Translate from 'react-translate-component';
-import apiCalls from '../../api/apiCalls';
+import React, { Component } from "react";
+import {
+	Drawer,
+	Tabs,
+	Collapse,
+	Typography,
+	Tooltip,
+	Modal,
+	Button,
+	Row, Col, Select, Button, Form
+} from "antd";
+import HistoryGridComponent from "./HistoryGridComponent";
+import { connect } from "react-redux";
+import Translate from "react-translate-component";
+import apiCalls from "../../api/apiCalls";
+import Info from "../shared/info";
 import List from "../grid.component";
 import {getTransactionSearch } from './api';
 const { Option } = Select;
@@ -25,15 +36,12 @@ class TransactionsHistory extends Component {
     };
     this.gridRef = React.createRef();
   }
-  
-  
-  componentDidMount() {
+
+componentDidMount() {
     this.TransactionSearch();
     // this.props.thref(this)
     // apiCalls.trackEvent({ "Type": 'User', "Action": 'Transactions All page view', "Username": this.props.member?.userName, "MemeberId": this.props.member?.id, "Feature": 'Transactions', "Remarks": 'Transactions All page view', "Duration": 1, "Url": window.location.href, "FullFeatureName": 'Transactions' });
   }
-
-  
   gridColumns = [
     {field: "date", title: "Date", filter: true, isShowTime: true, filterType: "date", locked: true, width: 210,
 },
@@ -93,19 +101,28 @@ class TransactionsHistory extends Component {
     });
 
   };
-  render() {
-    // const { BuySellURL, SwapURL, WithdrawURL, DepositURL, DepositCryptoURL, WithdrawCryptoURL } = this.state
-    const { Title } = Typography;
-    const { memberData, typeData, doctypeData, gridUrl, searchObj } = this.state;
-    const options1 = typeData.map((d) => (
-      <Option key={d.value} value={d.name}>{d.name}</Option>
-    ));
-    const options2 = doctypeData.map((d) => (
-      <Option key={d.value} value={d.name}>{d.name}</Option>
-    ));
-    return (
-      <>
-      <Drawer
+
+	
+	render() {
+		// const {
+		// 	BuySellURL,
+		// 	SwapURL,
+		// 	WithdrawURL,
+		// 	DepositURL,
+		// 	DepositCryptoURL,
+		// 	WithdrawCryptoURL,
+		// } = this.state;
+		const { Title } = Typography;
+		const { memberData, typeData, doctypeData, gridUrl, searchObj } = this.state;
+		const options1 = typeData.map((d) => (
+		  <Option key={d.value} value={d.name}>{d.name}</Option>
+		));
+		const options2 = doctypeData.map((d) => (
+		  <Option key={d.value} value={d.name}>{d.name}</Option>
+		));
+		return (
+			<>
+				 <Drawer
           title={[<div className="side-drawer-header">
             <Translate content="menu_transactions_history" component={Title} className="fs-26 fw-400 mb-0 text-white-30" />
 
@@ -179,112 +196,39 @@ class TransactionsHistory extends Component {
          columns={this.gridColumns}
          showExcelExport ={true}
         />
-        </Drawer>
-        {/* <Drawer code commented date: 06-05-2022
-          title={[<div className="side-drawer-header">
-            <Translate content="menu_transactions_history" component={Title} className="fs-26 fw-400 mb-0 text-white-30" />
-
-            <span onClick={this.props.onClose} className="icon md close-white c-pointer" />
-          </div>]}
-          placement="right"
-          closable={false}
-          width="100%"
-          onClose={this.props.onClose}
-          visible={this.props.showDrawer}
-          className="side-drawer-full custom-gridresponsive"
-        >
-          <div className="transaction-tabs">
-            <Tabs className="crypto-list-tabs mt-0" activeKey={this.state.activeTab} onChange={this.changeTab}>
-              <TabPane
-                key='1'
-                className="alltab-space"
-                onClick={() => this.changeTab("1")}
-                tab={<Translate content="All" component={Tabs.TabPane.tab} className="custom-font fw-400 fs-14" />}
-              >
-
-                {this.state.activeTab === '1' && <><Collapse onChange={collapseGrids} className="mb-16">
-                  <Panel
-                    header={<Translate content="BuyandSell" component={Collapse.Panel.header} className="custom-font fw-400 fs-14 text-white" />}
-                    key="1">
-                    {this.state.activeTab === '1' && <HistoryGridComponent columns={this.BuySellColmns} gridUrl={BuySellURL} params={{ memberId: this.props.member?.id }} ></HistoryGridComponent>}
-                  </Panel>
-                </Collapse>
-
-                  <Collapse onChange={collapseGrids} className="mb-16">
-                    <Panel
-                      header={<Translate content="menu_swap" component={Collapse.Panel.header} className="custom-font fw-400 fs-14 text-white " />}
-                      key="2">
-                      {this.state.activeTab === '1' && <HistoryGridComponent columns={this.SwapColmns} gridUrl={SwapURL} params={{ memberId: this.props.member?.id }}></HistoryGridComponent>}
-                    </Panel>
-                  </Collapse>
-
-                  <Collapse onChange={collapseGrids} className="mb-16">
-                    <Panel
-                      header={<Translate content="DepositandFiat" component={Collapse.Panel.header} className="custom-font fw-400 fs-14 text-white " />}
-                      key="3">
-                      {this.state.activeTab === '1' && <HistoryGridComponent columns={this.DepositColmns} gridUrl={DepositURL} params={{ memberId: this.props.member?.id }}></HistoryGridComponent>}
-                    </Panel>
-                  </Collapse>
-
-
-                  <Collapse onChange={collapseGrids} className="mb-16">
-                    <Panel
-                      header={<Translate content="DepositandCrypto" component={Collapse.Panel.header} className="custom-font fw-400 fs-14 text-white " />}
-                      key="4">
-                      {this.state.activeTab === '1' && <HistoryGridComponent columns={this.depositCryptoColomns} gridUrl={DepositCryptoURL} params={{ memberId: this.props.member?.id }}></HistoryGridComponent>}
-                    </Panel>
-                  </Collapse>
-
-                  <Collapse onChange={collapseGrids} className="mb-16">
-                    <Panel
-                      header={<Translate content="withdrawFiat" component={Collapse.Panel.header} className="custom-font fw-400 fs-14 text-white " />}
-                      key="5">
-                      {this.state.activeTab === '1' && <HistoryGridComponent columns={this.withdrawcolumns} gridUrl={WithdrawURL} params={{ memberId: this.props.member?.id }}></HistoryGridComponent>}
-                    </Panel>
-                  </Collapse>
-
-                  <Collapse onChange={collapseGrids} className="mb-16">
-                    <Panel
-                      header={<Translate content="withdrawCrypto" component={Collapse.Panel.header} className="custom-font fw-400 fs-14 text-white " />}
-                      key="6">
-                      {this.state.activeTab === '1' && <HistoryGridComponent columns={this.withdrwCryptoColomns} gridUrl={WithdrawCryptoURL} params={{ memberId: this.props.member?.id }}></HistoryGridComponent>}
-                    </Panel>
-                  </Collapse></>}
-              </TabPane>
-
-              <TabPane
-                tab={<Translate content="BuyandSell" component={Tabs.TabPane.tab} className="custom-font fw-400 fs-14" />}
-                key='2'
-                onClick={() => this.changeTab("2")}>
-                {this.state.activeTab === '2' && <HistoryGridComponent columns={this.BuySellColmns} gridUrl={BuySellURL} params={{ memberId: this.props.member?.id }}></HistoryGridComponent>}
-              </TabPane>
-              <TabPane
-                tab={<Translate content="menu_swap" component={Tabs.TabPane.tab} className="custom-font fw-400 fs-14" />}
-                key='3'
-                onClick={() => this.changeTab("3")}>
-                {this.state.activeTab === '3' && <HistoryGridComponent columns={this.SwapColmns} gridUrl={SwapURL} params={{ memberId: this.props.member?.id }}></HistoryGridComponent>}
-              </TabPane>
-              <TabPane tab={<Translate content="DepositandFiat" component={Tabs.TabPane.tab} className="custom-font fw-400 fs-14" />} key='4' onClick={() => this.changeTab("4")}>
-                {this.state.activeTab === '4' && <HistoryGridComponent columns={this.DepositColmns} gridUrl={DepositURL} params={{ memberId: this.props.member?.id }}></HistoryGridComponent>}
-              </TabPane>
-              <TabPane tab={<Translate content="DepositandCrypto" component={Tabs.TabPane.tab} className="custom-font fw-400 fs-14 " />} key='5' onClick={() => this.changeTab("5")}>
-                {this.state.activeTab === '5' && <HistoryGridComponent columns={this.depositCryptoColomns} gridUrl={DepositCryptoURL} params={{ memberId: this.props.member?.id }}></HistoryGridComponent>}
-              </TabPane>
-              <TabPane tab={<Translate content="withdrawFiat" component={Tabs.TabPane.tab} className="custom-font fw-400 fs-14 " />} key='6' onClick={() => this.changeTab("6")}>
-                {this.state.activeTab === '6' && <HistoryGridComponent columns={this.withdrawcolumns} gridUrl={WithdrawURL} params={{ memberId: this.props.member?.id }}></HistoryGridComponent>}
-              </TabPane>
-              <TabPane tab={<Translate content="withdrawCrypto" component={Tabs.TabPane.tab} className="custom-font fw-400 fs-14" />} key='7' onClick={() => this.changeTab("7")}>
-                {this.state.activeTab === '7' && <HistoryGridComponent columns={this.withdrwCryptoColomns} gridUrl={WithdrawCryptoURL} params={{ memberId: this.props.member?.id }}></HistoryGridComponent>}
-              </TabPane>
-            </Tabs>
-          </div>
-
-        </Drawer> */}
-      </>
+				{/* <Modal
+					title="Crypto Currency"
+					visible={this.state.cryptoModal}
+					className="crypto-list"
+					destroyOnClose
+					closeIcon={
+						<Tooltip title="Close">
+							<span
+								className="icon md close-white c-pointer"
+								onClick={() =>
+									this.setState({ ...this.state, cryptoModal: false })
+								}
+							/>
+						</Tooltip>
+					}
+					footer={
+						<Button
+							className="primary-btn pop-btn"
+							onClick={() =>
+								this.setState({ ...this.state, cryptoModal: false })
+							}>
+							Close
+						</Button>
+					}>
+					<Info id={this.state.selectedId} type={this.state.selectedModal} />
+				</Modal> */}
+				</Drawer>
+			   </>
+			   
     );
   }
 }
 const connectStateToProps = ({ userConfig }) => {
-  return { member: userConfig.userProfileInfo }
-}
-export default connect(connectStateToProps)(TransactionsHistory)
+	return { member: userConfig.userProfileInfo };
+};
+export default connect(connectStateToProps)(TransactionsHistory);
