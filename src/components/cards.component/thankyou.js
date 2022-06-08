62c87550e864e3b105b01f76e990116c87ffbc39
saranya <saranya.ziraff@gmail.com>
2022-06-08 08:48:21 +0530

    design chnages

--- src/components/cards.component/thankyou.js
+++ src/components/cards.component/thankyou.js
@@ -17,18 +17,9 @@ class CardStatus extends Component {
     render() {
         return (
             <div className="main-container mt-36">
-<<<<<<< HEAD
-            <div className="text-center submission-sec">
-          
-            <Image width={80} preview={false} src={success} />
-            <Title className="text-white" level={2}>Your Suisse Card Application Is Successfully Completed</Title>
-            <Text className="text-white">Introducing The SuisseBase Rewards Visa® Signature Credit Card. The World's First Rewards Credit Card.Introducing The SuisseBase Rewards Visa® Signature Credit Card. The World's First Rewards Credit Card.</Text>
-                    <div className="my-25"><Button type="primary" className="pop-btn text-textDark">BACK TO DASHBORD</Button>
-					</div>
-          </div>
-          </div>
-=======
                 <div className="text-center submission-sec">
+                    {/* <Image width={80} preview={false} src={inprogress} />
+                    <Image width={80} preview={false} src={rejected} /> */}
                     <Image width={80} preview={false} src={this.state.images[this.props.status?.toLowerCase()] || success} />
                     <Title level={2}>{this.state.messages[this.props.status?.toLowerCase()]} </Title>
                     <Text>Introducing The SuisseBase Rewards Visa® Signature Credit Card. The World's First Rewards Credit Card.Introducing The SuisseBase Rewards Visa® Signature Credit Card. The World's First Rewards Credit Card.</Text>
@@ -36,7 +27,6 @@ class CardStatus extends Component {
                     </div>
                 </div>
             </div>
->>>>>>> 8b3ce4e837d19c256210abad4693a1a44949f0e0
         )
     }
 
