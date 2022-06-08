cfaa17d8502d749428a08dc41bc8a7ee14dcc8e5
saranya <saranya.ziraff@gmail.com>
2022-06-08 20:15:57 +0530

    design changes

--- src/components/cards.component/index.js
+++ src/components/cards.component/index.js
@@ -43,7 +43,14 @@ class NewCards extends Component {
           <Col span={10}>
             <Title level={3} className="fs-24 fw-600 mb-0 text-white px-4">New Card</Title>
             <Title level={2} className="mb-8 m-0 fw-600 text-white px-4">Suisse Card</Title>
-            <Text className="text-white">Introducing The SuisseBase Rewards Visa® Signature Credit Card. The World's First Rewards Credit Card.</Text>
+            <Text className="text-white">Change “New Card” to “A card you control” and bring it below “Suisse Card”</Text>
+            <Text className="text-white"> Change “Suisse Card” to “Suissebase Corporate Expense Card”</Text>
+            <Text className="text-white"> Change “Introducing…..Credit Card” to “Powered by Mastercard, a global pioneer in payment innovation and technology connecting billions of consumers, issuers, merchants, governments & businesses worldwide.”</Text>
+            
+            <Title level={3} className="mb-8 fw-600 text-white px-4">The card offers a myriad range of features:</Title>
+            <Text className="text-white">Top up and manage your corporate’s debit card all through the Suissebase platform</Text>
+            <Text className="text-white">Issue your employees physical and virtual cards for secure local and international payments</Text>
+            <Text className="text-white">Stay in control of the card’s security all with a tap of your fingers</Text>
             <div className="d-flex mt-24">
               <Button loading={this.state.loading} type="primary" className="pop-btn text-textDark" onClick={()=>this.applyCard()}>Apply Now</Button>
               {/* <Button type="primary" className="btn-back ml-16">Back</Button> */}
--- src/components/cards.component/thankyou.js
+++ src/components/cards.component/thankyou.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Image, Typography, Button } from "antd";
 import success from "../../assets/images/success.png";
+import pending from "../../assets/images/pending.png";
+import reject from "../../assets/images/reject.png";
 const { Title, Text } = Typography;
 
 class CardStatus extends Component {
@@ -18,14 +20,32 @@ class CardStatus extends Component {
         return (
             <div className="main-container mt-36">
                 <div className="text-center submission-sec">
-                    {/* <Image width={80} preview={false} src={inprogress} />
-                    <Image width={80} preview={false} src={rejected} /> */}
+                    {/* <Image width={80} preview={false} src={pending} />
+                    <Image width={80} preview={false} src={reject} /> */}
                     <Image width={80} preview={false} src={this.state.images[this.props.status?.toLowerCase()] || success} />
-                    <Title level={2}>{this.state.messages[this.props.status?.toLowerCase()]} </Title>
-                    <Text>Introducing The SuisseBase Rewards Visa® Signature Credit Card. The World's First Rewards Credit Card.Introducing The SuisseBase Rewards Visa® Signature Credit Card. The World's First Rewards Credit Card.</Text>
-                    <div className="my-25"><Button onClick={() => this.props.onBack()} type="primary" className="pop-btn text-textDark">BACK TO DASHBORD</Button>
+                    <Title level={2} className="text-white">{this.state.messages[this.props.status?.toLowerCase()]} </Title>
+                    <Text className="text-white">Change “Your Suisse… Completed” to “Application for Suissebase Corporate Expense Card Is Approved”</Text>
+                    <Text className="text-white">Change “Back to dashboard” to “Let’s get started”</Text>
+                    <Text className="text-white">Your application for the Corporate Expense Card has been successful. To access the admin dashboard for the cards portal, please navigate to the Cards tab above. You should have received an email with the card’s admin credential and further instructions on navigating your way to the platform.</Text>
+                    <div className="my-25 text-textDark"><Button onClick={() => this.props.onBack()} type="primary" className="pop-btn text-textDark">BACK TO DASHBORD</Button>
                     </div>
                 </div>
+{/* For Pending */}
+                {/* <div className="text-center submission-sec">
+                    <Image width={80} preview={false} src={pending} />
+                    <Title level={2} className="text-white"> </Title>
+                    <Text className="text-white">Application for Suissebase Corporate Expense Card Is Under Review</Text>
+                    <div className="my-25 text-textDark"><Button onClick={() => this.props.onBack()} type="primary" className="pop-btn text-textDark">BACK TO DASHBORD</Button>
+                    </div>
+                </div> */}
+{/* For Reject */}
+                {/* <div className="text-center submission-sec">
+                    <Image width={80} preview={false} src={reject} />
+                    <Title level={2} className="text-white"> </Title>
+                    <Text className="text-white">Approved cancel, Please contact admin</Text>
+                    <div className="my-25 text-textDark"><Button onClick={() => this.props.onBack()} type="primary" className="pop-btn text-textDark">BACK TO DASHBORD</Button>
+                    </div>
+                </div> */}
             </div>
         )
     }
