da3169e86bc66e47634aaa6b35d658aadd82789e
saranya <saranya.ziraff@gmail.com>
2022-06-13 17:29:07 +0530

    cards design chnages

--- src/components/cards.component/thankyou.js
+++ src/components/cards.component/thankyou.js
@@ -8,16 +8,19 @@ const { Title, Text } = Typography;
 class CardStatus extends Component {
     state = {
         images: {
+            success: success,
             inprogress: inprogress,
             rejected: rejected,
         },
         messages: {
-            inprogress: 'Application for Suissebase Corporate Expense Card Is Under Review',
-            rejected: "Application for Suissebase Corporate Expense Card Is Rejected, Please contact admin"
+            success: 'We are currently reviewing your application and we will be in touch soon regarding the outcome of your application. If you have any questions, please contact our support at support@suissebase.ch',
+            inprogress: 'We are in the process of reviewing the information for your application. We will notify you via email about the outcome of your application. If you have any questions, please contact our support at support@suissebase.ch ',
+            rejected: "We regret to inform you that we are presently unable to approve your application Suissebase Corporate Expense Card. If you have any questions, please contact our support at support@suissebase.ch "
         },
         titles:{
-            inprogress: 'In-Progress',
-            rejected:'Rejected'
+            success: 'Thank you for applying for Suissebase Corporate Expense Card!',
+            inprogress: 'Suissebase Corporate Expense Card Application Status ',
+            rejected:'Suissebase Corporate Expense Card Application Status '
         }
     }
     render() {
@@ -25,7 +28,7 @@ class CardStatus extends Component {
             <div className="main-container">
                 <div className="text-center submission-sec">
                     <Image width={80} preview={false} src={this.state.images[this.props.status?.toLowerCase()] || success} />
-                    <Title level={2} className="text-white mt-16 mb-0 pb-8">{this.state.titles[this.props.status?.toLowerCase()]} </Title>
+                    <Title level={2} className="text-white my-16 mb-0">{this.state.titles[this.props.status?.toLowerCase()]} </Title>
                     <Text className="text-white-30">{this.state.messages[this.props.status?.toLowerCase()]}</Text>
                     <div className="my-25"><Button onClick={() => this.props.onBack()} type="primary" className="mt-36 pop-btn text-textDark">BACK TO DASHBORD</Button>
                     </div>
