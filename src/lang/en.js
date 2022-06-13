1a7b14914f366778ba9fb40d2065657438347258
rao <rao@tlvfintech.com>
2022-06-13 06:45:43 +0000

    conflicts

--- src/lang/en.js
+++ src/lang/en.js
@@ -563,4 +563,8 @@ export default {
 	business_account:"Business Account",
 	empty: " ",
 	Recipient_Business_name: "Recipient Full Name/Business Name",
+	cards:"Cards",
+	agree_to_signup: "By clicking Sign Up, I here by acknowledge that I agree to Suissebase's %(link)s Agreement and I've read the Privacy Policy",
+	terms_Use: "Terms of Use",
+	"whitelist":"Declaration form"
 };
