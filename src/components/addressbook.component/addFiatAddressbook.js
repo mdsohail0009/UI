9c6d44a4cb40e23054fe01e0e5f2910631d3a6a0
rao <rao@tlvfintech.com>
2022-06-13 06:43:49 +0000

    conflicts

--- src/components/addressbook.component/addFiatAddressbook.js
+++ src/components/addressbook.component/addFiatAddressbook.js
@@ -1149,6 +1149,9 @@ const NewFiatAddress = (props) => {
 									marginBottom: "10px",
 								}}
 							/>
+								{/* <div className="whitelist-note">
+							<Alert type="warning" message={`Note : Declaration form has been sent to ${fiatAddress?.email||"your email address"}. Please sign using link received in email to whitelist your address`} showIcon closable={false} />
+							</div> */}
 						</div>
 
 						<Form.Item className="text-center">
