710d24455220778c2811c61c20a99509c4e1d43e
Naresh <naresh@tlvfintech.com>
2022-06-20 16:30:54 +0530

    changes

--- src/utils/state.connect.js
+++ src/utils/state.connect.js
@@ -1,6 +1,6 @@
 const { connect } = require("react-redux")
 
-const mapStateToProps = ({ oidc, buySell, sendReceive, swapStore, buyFiat,buyInfo,userConfig, dashboard,addressBookReducer, cardsStore, TransforStore }) => {
+const mapStateToProps = ({ oidc, buySell, sendReceive, swapStore, buyFiat,buyInfo,userConfig, dashboard,addressBookReducer, TransforStore,cardsStore  }) => {
     return { profile: oidc.profile, user: oidc.user, buySell, sendReceive, swapStore, buyFiat,sellData:buyInfo,userProfile:userConfig.userProfileInfo,dashboard:dashboard,addressBookReducer,twoFA:userConfig.twoFA,cardsStore:cardsStore, TransforStore:TransforStore }
 }
 const mapDispatchToProps = dispatch => {
