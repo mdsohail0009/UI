7810ddd4e71df7ee1e78fc52599acedde9091339
Naresh <naresh@tlvfintech.com>
2022-06-21 16:50:01 +0530

    transfor changes

--- src/components/transfor.component/transforSuccess.js
+++ src/components/transfor.component/transforSuccess.js
@@ -21,7 +21,7 @@ class TransforSuccessMsg extends Component {
                 <div className="success-pop text-center pt-24">
                     <img src={success} className="confirm-icon" alt={"success"} />
                     <Translate content="success_msg" component={Title} className="text-white-30 fs-36 fw-200 mb-4" />
-                    <Paragraph className="fs-14 text-white-30 fw-200"><Translate content="sucessText1" component={Text} className="fs-14 text-white-30 fw-200" /> {this.props.transforObj.transferAmount} {this.props.transforObj.walletCode} <Translate content="sucessText2" className="fs-14 text-white-30 fw-200" component={Text} /></Paragraph>
+                    <Paragraph className="fs-14 text-white-30 fw-200"><Translate content="sucessText1" component={Text} className="fs-14 text-white-30 fw-200" /> {this.props.transforObj?.transferAmount} {this.props.transforObj?.walletCode} <Translate content="sucessText2" className="fs-14 text-white-30 fw-200" component={Text} /></Paragraph>
                     {/* <Translate content="success_decr" component={Paragraph} className="fs-16 text-white-30 fw-200" /> */}
                     <Space direction="vertical" size="large">
                         <Translate content="return_to_transfor" className="f-16 text-white-30 mt-16 text-underline" component={Link} onClick={() => {this.props.changeStep("tranforcoin");this.props.clearData(null)}} />
