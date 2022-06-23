c489bddcbe6272ed66576c9e62f0eced705413d5
rao <rao@tlvfintech.com>
2022-06-23 09:19:20 +0000

    console remove

--- src/layout/header.component.js
+++ src/layout/header.component.js
@@ -806,8 +806,6 @@ class Header extends Component {
                 this.props.dispatch(setHeaderTab(key.key));
               }}
             >
-             {console.log(this.props.userConfig.isBusiness && process.env.REACT_APP_ISSHOWCARDS == true)}
-              {console.log('process.env.REACT_APP_ISSHOWCARDS: '+process.env.REACT_APP_ISSHOWCARDS)}
               {this.props.userConfig.isBusiness && process.env.REACT_APP_ISSHOWCARDS == "true" && <Translate
                 content="cards"
                 component={Menu.Item}
