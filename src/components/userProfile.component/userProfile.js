8ead4e51388b347438da9abff556a1bae850407a
Subbareddy <subbareddy@zirafftechnologies.com>
2022-06-23 16:16:45 +0530

    keyvalue

--- src/components/userProfile.component/userProfile.js
+++ src/components/userProfile.component/userProfile.js
@@ -84,8 +84,8 @@ class UserProfile extends Component {
                         {this.state.activeTab == 7 && <Referral />}
                     </TabPane>
                     <TabPane tab={<span><span className="icon lg bell-icon mr-16" />
-                    <Translate content="notification" className="f-16  mt-16" /></span>} key="7" >
-                        {this.state.activeTab == 7 && <NotificationScreen />}
+                    <Translate content="notification" className="f-16  mt-16" /></span>} key="8" >
+                        {this.state.activeTab == 8 && <NotificationScreen />}
                         </TabPane>
 
                 </Tabs>
