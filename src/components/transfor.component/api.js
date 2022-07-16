9a88a4b18953948076fcb5123192d95872ce0205
naresh <rao@tlvfintech.com>
2022-07-16 15:03:45 +0530

    call changes

--- src/components/transfor.component/api.js
+++ src/components/transfor.component/api.js
@@ -2,14 +2,14 @@ import { apiClient, ipRegistry } from "../../api";
 import { ApiControllers } from "../../api/config"
 
 
-const gettransforLu = (memID) => {
+const gettransforLu = (customerId) => {
 	return apiClient.get(ApiControllers.common + `PartnerWalletTypesLU`);
 };
-const getCurrencyLu = (memID) => {
-	return apiClient.get(ApiControllers.earn + `ListofCurrencies/${memID}` );
+const getCurrencyLu = (customerId) => {
+	return apiClient.get(ApiControllers.earn + `ListofCurrencies/${customerId}` );
 };
-const getcurrencyBalance = (memID,fromwallet,currency) => {
-	return apiClient.get(ApiControllers.earn + `AvailableBalance/${memID}/${fromwallet}/${currency}` );
+const getcurrencyBalance = (customerId,fromwallet,currency) => {
+	return apiClient.get(ApiControllers.earn + `AvailableBalance/${customerId}/${fromwallet}/${currency}` );
 };
 const saveTransfor = (obj) => {
 	return apiClient.post(ApiControllers.earn + `SaveTransfer`,obj );
