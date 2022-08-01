45fa231df9e09e946adb1da40c400d2207c3ac9d
naresh@tlvfintech.com <nagarajuk@zirafftechnologies.com>
2022-08-01 16:04:50 +0530

    billpayments permissions implementation

--- src/components/shared/permissions/permissionService.js
+++ src/components/shared/permissions/permissionService.js
@@ -3,6 +3,6 @@ import { store } from "../../../store"
 const getFeatureId = (path) => {
     const { menuItems: { features } } = store.getState();
     const feature = features?.data?.find(item => item.path == path);
-    return feature.id;
+    return feature?.id;
 }
 export { getFeatureId };
