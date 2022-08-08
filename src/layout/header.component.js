d9291380e8f0bdb0fba29653265f18926dd3e4da
naresh@tlvfintech.com <nagarajuk@zirafftechnologies.com>
2022-08-08 09:59:15 +0530

    conflicts resolve

--- src/layout/header.component.js
+++ src/layout/header.component.js
@@ -162,7 +162,7 @@ class Header extends Component {
           <ul className="pl-0 drpdwn-list">
             <li
             >
-              <Popover placement="left" content={<><div onClick={() => window.open("https://pyrros.instance.kyc-chain.com/#/auth/signup/6120197cdc204d9ddb882e4d")}>
+              <Popover placement="left" content={<><div className="iban-hov" onClick={() => window.open("https://pyrros.instance.kyc-chain.com/#/auth/signup/6120197cdc204d9ddb882e4d")}>
                 <Link>
                   <Translate
                     content="personal_account"
@@ -170,7 +170,7 @@ class Header extends Component {
                     className="text-white-30" key="1"
                   />
                 </Link><span className="icon c-pointer md rarrow-white ml-12" /></div>
-                <div onClick={() => window.open("https://pyrros.instance.kyc-chain.com/#/auth/signup/611b3ed20414885a6fc64fa7")}>
+                <div className="iban-hov" onClick={() => window.open("https://pyrros.instance.kyc-chain.com/#/auth/signup/611b3ed20414885a6fc64fa7")}>
                   <Link>
                     <Translate
                       content="business_account"
