686a1dcf3d4a31a4b4d6e3aae9d347ec7b9197fb
Nagaraju <Integration1@yellowblock.net>
2022-08-11 15:02:09 +0530

    added feature permissions

--- src/components/shared/permissions/permissionService.js
+++ src/components/shared/permissions/permissionService.js
@@ -1,8 +1,24 @@
+import { fetchFeaturePermissions } from "../../../reducers/feturesReducer";
 import { store } from "../../../store"
+import { KEY_URL_MAP } from "./config";
 
 const getFeatureId = (path) => {
     const { menuItems: { features } } = store.getState();
     const feature = features?.data?.find(item => item.path == path);
     return feature?.id;
 }
-export { getFeatureId };
+const getFeatureWithKeyId = (key) => {
+    const { menuItems: { features } } = store.getState();
+    const feature = features?.data?.find(item => item.key == key);
+    return feature?.id;
+}
+const getFeaturePermissionsByKey = (key,callback) => {
+    const {userConfig:{userProfileInfo},menuItems:{featurePermissions}}=store.getState();
+    const fid = getFeatureWithKeyId(key);
+    if(featurePermissions[KEY_URL_MAP[key]]){
+        callback();
+    }else{
+        store.dispatch(fetchFeaturePermissions(fid,userProfileInfo.id,callback));
+    }
+}
+export { getFeatureId, getFeatureWithKeyId,getFeaturePermissionsByKey };
