1d2af853fd386ce761de8f5a98153fed5c4ffe96
Nagaraju <Integration1@yellowblock.net>
2022-08-11 15:02:42 +0530

    route

--- src/components/shared/permissions/protected.route.js
+++ src/components/shared/permissions/protected.route.js
@@ -1,14 +1,24 @@
 import { Route } from "react-router-dom"
+import { fetchFeaturePermissions } from "../../../reducers/feturesReducer";
 import { store } from "../../../store"
+import AccessDenied from "./access.denied";
 import { KEY_URL_MAP } from "./config";
 
 const ProtectedRoute = ({ component: Component, ...rest }) => {
-    const { menuItems: { features, featurePermissions } } = store.getState();
+    const { menuItems: { features, featurePermissions }, userConfig: { userProfileInfo } } = store.getState();
     return <Route {...rest} render={(props) => {
-        if (featurePermissions[KEY_URL_MAP][props.path]){
-            return <Component {...rest} />
-        }else{
-            
+        if (featurePermissions[KEY_URL_MAP][props.path]) {
+
+        } else {
+            const featureId = features.data?.find(feature => feature.key === rest.key)?.id;
+            store.dispatch(fetchFeaturePermissions(featureId, userProfileInfo.id, () => {
+                const view = featurePermissions?.actions?.find(permission => permission.permissionName === "view").values;
+                if (view) {
+                    return <Component {...rest} />
+                } else {
+                    <AccessDenied />
+                }
+            }));
         }
     }} />
 }
--- src/config/protected.route.js
+++ src/config/protected.route.js
@@ -3,15 +3,12 @@ import { Route } from "react-router-dom";
 import Loader from "../Shared/loader";
 
 const ProtectedRoute = ({ component: Component, ...props }) => {
-
-debugger
     if (props.featurePermissions.loading) {
         return <Loader />
     }
     return <Route path={props.path} component={Component} />
 }
 const connectStateToProps = ({ menuItems }) => {
-
     return { featurePermissions: menuItems.featurePermissions }
 }
 export default connect(connectStateToProps)(ProtectedRoute);
