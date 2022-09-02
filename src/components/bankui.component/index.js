eb22510d181915debd004a0958fe30f76a109979
Naresh <naresh@zirafftechnologies.com>
2022-09-02 20:07:54 +0530

    create account

--- src/components/bankui.component/index.js
+++ src/components/bankui.component/index.js
@@ -30,13 +30,13 @@ class BankWallets extends Component {
         }
     }
     
-    createAccount= (e)=>{
-        if(this.props.userProfile.isBusiness){
-            window.open(`http://localhost:3001/businessCreateAccount/${e}`)
-        }else{
-            window.open(`http://localhost:3001/createAccount/${e}`)
-        }
-    }
+    // createAccount= (e)=>{
+    //     if(this.props.userProfile.isBusiness){
+    //         window.open(`http://localhost:3001/businessCreateAccount/${e}`)
+    //     }else{
+    //         window.open(`http://localhost:3001/createAccount/${e}`)
+    //     }
+    // }
       menuBar = (item) => (
         <Menu>
               <ul className="pl-0 drpdwn-list">
@@ -129,10 +129,10 @@ class BankWallets extends Component {
                             //     window.open(process.env.REACT_APP_BANK_UI_URL, "_blank")
                             //   }
                             
-                              onClick={()=>this.createAccount(item.currency)}
-                              // onClick={() =>
-                              //   window.open(`http://localhost:3001/createAccount/${item.currency}`)
-                              // }
+                              //onClick={()=>this.createAccount(item.currency)}
+                              onClick={() =>
+                                window.open(`http://localhost:3001/feepage/${item.currency}`)
+                              }
                              />  
                             
                               </div> )}
