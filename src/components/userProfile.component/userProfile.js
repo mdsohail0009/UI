3e993313ac46fb9b65d98e32bfc52129606c82f3
Nagaraju K <nagarajuk@zirafftechnologies.com>
2022-09-06 15:16:55 +0530

    notifications enable

--- src/components/userProfile.component/userProfile.js
+++ src/components/userProfile.component/userProfile.js
@@ -82,10 +82,10 @@ class UserProfile extends Component {
                     <Translate content="referr" className="f-16  mt-16" /></span>} key="7" >
                         {this.state.activeTab == 7 && <Referral />}
                     </TabPane>
-                    {/* <TabPane tab={<span><span className="icon lg notification-icon mr-16" />
+                    <TabPane tab={<span><span className="icon lg notification-icon mr-16" />
                     <Translate content="notification" className="f-16  mt-16" /></span>} key="8" >
                         {this.state.activeTab == 8 && <NotificationScreen />}
-                        </TabPane> */}
+                        </TabPane>
                 </Tabs>
             </div>
             <div className="main-container visible-mobile">
