dab144173729fdc8dce32990c0122c7252b0c987
naresh@tlvfintech.com <sowjanya.ziraff>
2022-09-12 21:36:17 +0530

    changes

--- src/components/onthego.transfer/Myself/index.js
+++ src/components/onthego.transfer/Myself/index.js
@@ -192,7 +192,7 @@ const MyselfNewTransfer = ({ currency, isBusiness,onTheGoObj, ...props }) => {
                     />
             </Form.Item>
         </Col>
-            {currency == 'EUR' && !isBusiness && <Col xs={24} md={12} lg={12} xl={12} xxl={12}>
+            {/* {currency == 'EUR' && !isBusiness && <Col xs={24} md={12} lg={12} xl={12} xxl={12}>
                 <Form.Item
                     className="custom-forminput custom-label mb-0"
                     name="iban"
@@ -217,7 +217,8 @@ const MyselfNewTransfer = ({ currency, isBusiness,onTheGoObj, ...props }) => {
                         placeholder='IBAN'
                         onChange={(e)=>getBankDeails(e)}/>
                 </Form.Item>
-            </Col>}</Row>
+            </Col>} */}
+            </Row>
             <Translate style={{ fontSize: 18 }}
                     content="Beneficiary_Details"
                     component={Paragraph}
@@ -273,7 +274,7 @@ const MyselfNewTransfer = ({ currency, isBusiness,onTheGoObj, ...props }) => {
         </div>
 
         <Paragraph className="mb-8  text-white fw-500 mt-16" style={{ fontSize: 18 }}>Bank Details</Paragraph>
-        {currency == 'EUR' && isBusiness && <Col xs={24} md={12} lg={12} xl={12} xxl={12}>
+        {currency == 'EUR' && <Col xs={24} md={12} lg={12} xl={12} xxl={12}>
             <Form.Item
                 className="custom-forminput custom-label mb-0"
                 name="iban"
