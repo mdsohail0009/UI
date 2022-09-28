497fc4f7f8686e0938d1cee38106ec5cbe60e6ef
subbareddy <nagarajuk@zirafftechnologies.com>
2022-09-28 17:29:52 +0530

    butron hide

--- src/components/onthego.transfer/Myself/index.js
+++ src/components/onthego.transfer/Myself/index.js
@@ -372,13 +372,13 @@ const MyselfNewTransfer = ({ currency, isBusiness,onTheGoObj, ...props }) => {
                
             </div>
         </Col>}
-        <Col xs={24} md={10} lg={10} xl={10} xxl={10}>
+        {currency == 'EUR' &&<Col xs={24} md={10} lg={10} xl={10} xxl={10}>
             <Button className={`pop-btn dbchart-link fs-14 fw-500`} style={{width:"150px",marginTop:"32px",height:"42px"}}
                 loading={isValidateLoading}
                 onClick={() => onIbanValidate(enteredIbanData)} >
                 <Translate content="validate" />
             </Button>      
-        </Col>
+        </Col>}
 
                         
                            
