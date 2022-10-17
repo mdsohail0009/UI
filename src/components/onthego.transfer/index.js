2e4e348118b6627aef194116680448038fe23134
rao <rao@tlvfintech.com>
2022-10-17 19:52:39 +0530

    user issues resolves

--- src/components/onthego.transfer/index.js
+++ src/components/onthego.transfer/index.js
@@ -504,7 +504,7 @@ class OnthegoFundTransfer extends Component {
                             }}>
                                 <Col xs={6} md={2} lg={2} xl={3} xxl={3} className=""><div class="fund-circle text-white">{item?.name.charAt(0).toUpperCase()}</div></Col>
                                 <Col xs={14} md={24} lg={24} xl={19} xxl={19} className="small-text-align">
-                                    <label className="fs-16 fw-600 text-white l-height-normal text-captz">{item.name}</label>
+                                    <label className="fs-16 fw-600 text-white l-height-normal text-captz c-pointer">{item.name}</label>
                                     {item.accountNumber && <div><Text className="fs-14 text-white-30 m-0">{this.state.selectedCurrency} account ending with {item.accountNumber?.substr(item.accountNumber.length - 4)}</Text></div>}
                                 </Col>
                                 <Col xs={4} md={24} lg={24} xl={2} xxl={2} className="mb-0 mt-8">
@@ -539,7 +539,7 @@ class OnthegoFundTransfer extends Component {
                             }}>
                                 <Col xs={6} md={2} lg={2} xl={3} xxl={3} className=""><div class="fund-circle text-white">{item?.name.charAt(0).toUpperCase()}</div></Col>
                                 <Col xs={14} md={24} lg={24} xl={19} xxl={19} className=" small-text-align">
-                                    <label className="fs-16 fw-600 text-white l-height-normal text-captz">{item.name}</label>
+                                    <label className="fs-16 fw-600 text-white l-height-normal text-captz c-pointer">{item.name}</label>
                                     <div><Text className="fs-14 text-white-30 m-0">{this.state.selectedCurrency} account ending with {item.accountNumber?.substr(item.accountNumber?.length - 4)}</Text></div>
                                 </Col>
                                 <Col xs={4} md={24} lg={24} xl={2} xxl={2} className="mb-0 mt-8">
