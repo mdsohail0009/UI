7de643b79c8862af17b36c53949bae97c7f560b2
subbareddy <nagarajuk@zirafftechnologies.com>
2022-10-31 15:28:32 +0530

    console issue

--- src/components/sell.component/sellCrypto.js
+++ src/components/sell.component/sellCrypto.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Typography, Card, Empty,Image } from 'antd';
-import { setSelectedSellCoin, setStep } from '../../reducers/buysellReducer';
+import { setSelectedSellCoin, setSellHeaderHide, setStep } from '../../reducers/buysellReducer';
 import { connect } from 'react-redux';
 import { setCoinWallet, updateCoinDetails } from '../../reducers/buy.reducer';   // do not remove this line time being i will check // subbareddy
 import Loader from '../../Shared/loader'
