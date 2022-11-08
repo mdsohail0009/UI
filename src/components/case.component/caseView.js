de875ff230997fccdb71e7d108a618b721a10db3
Nagaraju K <nagarajuk@tlvfintech.com>
2022-11-08 10:55:07 +0000

    Updated caseView.js

--- src/components/case.component/caseView.js
+++ src/components/case.component/caseView.js
@@ -470,8 +470,7 @@ class RequestedDocs extends Component {
 												/>
 											</div>
 										)}
-                                    <Dragger accept=".pdf,.jpg,.jpeg,.png, .PDF, .JPG, .JPEG, .PNG" className="upload mt-4" multiple={false} action={process.env.REACT_APP_UPLOAD_API + "UploadFile"} showUploadList={false} beforeUpload={(props) => { this.beforeUpload(props) }} onChange={(props) => { this.handleUpload(props, doc) }}
-                                      headers={{Authorization : `Bearer ${this.props.user.access_token}`}}>
+                                    <Dragger accept=".pdf,.jpg,.jpeg,.png, .PDF, .JPG, .JPEG, .PNG" className="upload mt-4" multiple={false} action={process.env.REACT_APP_UPLOAD_API + "UploadFile"} showUploadList={false} beforeUpload={(props) => { this.beforeUpload(props) }} onChange={(props) => { this.handleUpload(props, doc) }}>
                                         <p className="ant-upload-drag-icon">
                                             <span className="icon xxxl doc-upload" />
                                         </p>
@@ -518,7 +517,7 @@ class RequestedDocs extends Component {
             </div></>;
     }
 }
-const mapStateToProps = ({ userConfig,oidc }) => {
-    return { userProfileInfo: userConfig.userProfileInfo, trackAuditLogData: userConfig.trackAuditLogData,user: oidc.user }
+const mapStateToProps = ({ userConfig }) => {
+    return { userProfileInfo: userConfig.userProfileInfo, trackAuditLogData: userConfig.trackAuditLogData }
 }
 export default connect(mapStateToProps)(RequestedDocs);
