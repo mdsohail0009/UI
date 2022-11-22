1a28a006f30f3a8f21b2a30994edc4d649d08d5d
Nagaraju K <nagarajuk@tlvfintech.com>
2022-11-22 12:15:51 +0000

    bank hide based on condition

--- src/components/dashboard.component/index.js
+++ src/components/dashboard.component/index.js
@@ -71,9 +71,9 @@ class Home extends Component {
 
                 <Row justify="center mt-16">
                 {this.state.permissions?.Balances && <Col xs={24} md={12} xl={10}>
-                    <div className="markets-panel">
+                   {this.props.userProfileInfo?.businessName && <div className="markets-panel">
                         <BankWallets/>
-                    </div>
+                    </div>}
                         <div className="markets-panel mb-16 markets-line">
                             <Wallets />
                         </div>
