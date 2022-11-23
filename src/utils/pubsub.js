0079363a756c2269b8b46c913bc1b525ecebb944
subbareddy <subbareddy@yellowblock.net>
2022-11-23 15:53:21 +0530

    mobile logout issue

--- src/utils/pubsub.js
+++ src/utils/pubsub.js
@@ -4,6 +4,8 @@ const coinSubject = new Subject();
 const publishBalanceRfresh = (bal) => coinSubject.next(bal);
 const amountSwapSubject = new Subject();
 const publishAmountSwap = (val) => amountSwapSubject.next(val);
-const dashboardTransactionSub=new Subject();
-const publishTransactionRefresh=()=>dashboardTransactionSub.next()
-export { publishBalanceRfresh, coinSubject,amountSwapSubject,publishAmountSwap,publishTransactionRefresh,dashboardTransactionSub }
+const dashboardTransactionSub = new Subject();
+const publishTransactionRefresh = () => dashboardTransactionSub.next();
+const headerSubscriber = new Subject();
+const handleHeaderProfileMenuClick = (menuitem, menuKey) => headerSubscriber.next({menuitem, menuKey});
+export { publishBalanceRfresh, coinSubject, amountSwapSubject, publishAmountSwap, publishTransactionRefresh, dashboardTransactionSub,headerSubscriber,handleHeaderProfileMenuClick }
