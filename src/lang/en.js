8d4d851ad2438d51511da38eb2dbb7573ce65c29
Naresh <naresh@tlvfintech.com>
2022-11-30 17:03:27 +0530

    conflicts resolve

--- src/lang/en.js
+++ src/lang/en.js
@@ -669,7 +669,7 @@ export default {
 	contact_amount_text: "Please contact support for higher amounts.",
 	validate: "Validate",
 	sell_select_currency: "SELECT CURRENCY TO RECEIVE",
-	transactions_history: "Transaction History",
+	transactions_history: "Transactions History",
 	transaction:"Transaction",
 	
 };
