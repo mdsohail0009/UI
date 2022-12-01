f5ec53118edcd91cc2557865997ee9645cd7e142
Naresh <naresh@tlvfintech.com>
2022-12-01 11:56:44 +0530

    changes

--- src/components/transactions.history.component/transaction.slips.js
+++ src/components/transactions.history.component/transaction.slips.js
@@ -96,7 +96,7 @@ class TransactionSlips extends Component {
             {!loader&&transactionSlipData && Object?.keys(transactionSlipData)?.map((key) => <>
               <Col xs={24} sm={24} md={24} lg={24} xxl={24}>
                 <div className="pay-list py-4 transaction-details" style={{ alignItems: 'baseline' }} key={key}>
-                  <Title className="fs-14 text-white fw-400 text-captz">{typeof transactionSlipData[key] === "object" ? key : transactionSlipData[key]}</Title>
+                  <Title className="fs-14 text-white fw-400">{typeof transactionSlipData[key] === "object" ? key : transactionSlipData[key]}</Title>
                   <Title className="fs-14 text-white fw-500  text-right trn-sumtext">
                     {(viewData[key] === null || viewData[key] === "") ? '-' : (transactionSlipData[key] === 'Date') ? moment.utc(viewData[key]).local().format("DD/MM/YYYY hh:mm:ss A") : (typeof transactionSlipData[key] === "object") ? `${this.getCombinedValues(transactionSlipData[key])}` : viewData[key]}
                   </Title>
