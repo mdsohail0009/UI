4f905ea3ce99cb8318e270d950c960eea35112c9
nagarajK <nagaraj@yellowblock.net>
2022-12-05 20:40:27 +0530

    conflicts

--- src/components/transactions.history.component/index.js
+++ src/components/transactions.history.component/index.js
@@ -185,13 +185,13 @@ class TransactionsHistory extends Component {
   handleChange = (value, prop) => {
     var val = "";
     let { customerData, searchObj } = this.state;
-    if (prop == "customerId") {
-      let index = customerData.findIndex(function (o) { return o.name == value; });
+    if (prop === "customerId") {
+      let index = customerData.findIndex(function (o) { return o.name === value; });
       val = customerData[index].id;
     }
-    searchObj[prop] = prop == "customerId" ? val : value;
+    searchObj[prop] = prop === "customerId" ? val : value;
     this.setState({ ...this.state, searchObj });
-    if (prop == "currency") {
+    if (prop === "currency") {
       const searchVal = `${value ? value : "All"}`;
       this.setState({ ...this.state, searchObj: { ...this.state.searchObj, currency: searchVal || "All" } })
     }
@@ -263,15 +263,15 @@ class TransactionsHistory extends Component {
       this.formDateRef.current.resetFields();
     }
   }
-transactionModal= async (data)=>{
-  this.setState({ ...this.state, showModal:true,modalData:data,viewLoader:true ,
-    isLoading:false  })
-    let response = await transactionsView(data.id,data.docType);
-    if(response.ok){
-      this.setState({ ...this.state, showModal:true,viewData:response.data ,
-        isLoading:false,viewLoader:false  })
-    }
-}
+  transactionModal= async (data)=>{
+    this.setState({ ...this.state, showModal:true,modalData:data,viewLoader:true ,
+      isLoading:false  })
+      let response = await transactionsView(data.id,data.docType);
+      if(response.ok){
+        this.setState({ ...this.state, showModal:true,viewData:response.data ,
+          isLoading:false,viewLoader:false  })
+      }
+  }
 handleCancel=()=>{
   this.setState({ ...this.state, showModal:false,downloadError:"" })
 }
