698242f4af21bdbbb75f7d641b8fe4dd811964fc
subbareddy <subbareddy@yellowblock.net>
2022-12-06 11:05:53 +0530

    added development only devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
