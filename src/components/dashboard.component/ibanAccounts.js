78f3f145b90680dc9b63b8a7030411b5d973ebe3
rao <rao@tlvfintech.com>
2022-12-06 17:53:59 +0530

    design chnages

--- src/components/dashboard.component/ibanAccounts.js
+++ src/components/dashboard.component/ibanAccounts.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
-import { Card, Typography,Row,Col } from 'antd';
-
+import { Card, Typography,Row,Col,Avatar, List } from 'antd';
+const data = [
+    {
+      title: 'Ant Design Title 1',
+    },
+    {
+      title: 'Ant Design Title 2',
+    },
+  ];
 const { Text,Title, Paragraph } = Typography;
 class SbCard extends Component {
 
@@ -9,10 +16,22 @@ class SbCard extends Component {
                 <div className='sb-accounts'>
                    <Row gutter={[16,16]}>
                     <Col xs={24} md={12} className=''>
-                    
+                        <List
+                            itemLayout="horizontal"
+                            dataSource={data}
+                            renderItem={(item) => (
+                                <List.Item>
+                                    <List.Item.Meta
+                                        avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+                                        title={<a href="https://ant.design">{item.title}</a>}
+                                        description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                                    />
+                                </List.Item>
+                            )}
+                        />
                     </Col>
                     <Col xs={24} md={12} className=''>
-                    
+
                     </Col>
                    </Row>
                 </div>
