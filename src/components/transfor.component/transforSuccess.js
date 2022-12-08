c4b473113e4783c476328c8a1aa0fa2f504b69d4
Naresh <naresh@tlvfintech.com>
2022-12-08 13:18:16 +0530

    code smells

--- src/components/transfor.component/transforSuccess.js
+++ src/components/transfor.component/transforSuccess.js
@@ -22,7 +22,6 @@ class TransforSuccessMsg extends Component {
                     <img src={success} className="confirm-icon" alt={"success"} />
                     <Translate content="success_msg" component={Title} className="text-white-30 fs-36 fw-200 mb-4" />
                     <Paragraph className="fs-14 text-white-30 fw-200"><Translate content="transforsucessText1" component={Text} className="fs-14 text-white-30 fw-200" /> {this.props.transforObj?.transferAmount} {this.props.transforObj?.walletCode} <Translate content="sucessText2" className="fs-14 text-white-30 fw-200" component={Text} /></Paragraph>
-                    {/* <Translate content="success_decr" component={Paragraph} className="fs-16 text-white-30 fw-200" /> */}
                     <Space direction="vertical" size="large">
                         <Translate content="return_to_transfor" className="f-16 text-white-30 mt-16 text-underline" component={Link} onClick={() => {this.props.changeStep("tranforcoin");this.props.clearData(null)}} />
                     </Space>
