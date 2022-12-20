457ac0b37bc402fef30313ad29cafc0e7cdc1180
SubbaReddy Anduri <subbaiah@tlvfintech.com>
2022-12-20 12:08:37 +0000

    Updated app_config.js

--- src/utils/app_config.js
+++ src/utils/app_config.js
@@ -1,4 +1,4 @@
-const AppConfig = {
+const _config = {
     REACT_APP_API_END_POINT: process.env.REACT_APP_API_END_POINT,
     REACT_APP_REDIRECT_URI: process.env.REACT_APP_REDIRECT_URI,
     REACT_APP_CLIENT_ID: process.env.REACT_APP_CLIENT_ID,
