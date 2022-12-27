d4a3141fec057d13889302ed971cf37040cf203b
rao <rao@tlvfintech.com>
2022-12-27 19:19:38 +0530

    AB-onclick

--- src/components/toolbar.component/actions.toolbar.js
+++ src/components/toolbar.component/actions.toolbar.js
@@ -10,7 +10,7 @@ class ActionsToolbar extends Component {
                 {menuItems?.featurePermissions?.loading && <Spin />}
                 {!menuItems?.featurePermissions?.loading && <div>
                     <ul className="admin-actions mb-0 address-icons">
-                        {menuItems?.featurePermissions[featureKey]?.actions?.map(action => <>{(!["View", "view"].includes(action.permissionName) && (action.values)) && <Tooltip title={action.toolTip}><li onClick={() => onActionClick(action.permissionName)}><span className={`icon md c-pointer ${action.icon}-icon`}></span></li></Tooltip>}</>)}
+                        {menuItems?.featurePermissions[featureKey]?.actions?.map(action => <>{(!["View", "view"].includes(action.permissionName) && (action.values)) && <Tooltip title={action.toolTip}><li><span onClick={() => onActionClick(action.permissionName)} className={`icon md c-pointer ${action.icon}-icon`}></span></li></Tooltip>}</>)}
                     </ul>
                 </div>}
             </>
