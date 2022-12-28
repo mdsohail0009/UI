5a0ead9c0916ced9fdfef3d0aa3d47b7abb874a4
rao <rao@tlvfintech.com>
2022-12-28 17:51:17 +0530

    AB-batch-payments upload

--- src/components/batchpayment.component/uploadGrid.js
+++ src/components/batchpayment.component/uploadGrid.js
@@ -54,9 +54,9 @@ const BatchpaymentView = (props) => {
                 <div>
               {props.dataItem.beneficiarydetail?.map(item=>
                 <>
-                <div>
-                <span className="text-yellow gridLink"  onClick={() => docPreview(item)}>{item.documentName}</span>
-                 <span className="icon md close c-pointer" onClick={() => docDelete(item)} />
+                <div className='file-label d-flex justify-content mb-8 py-4'>
+                <div><span className="mb-0 fs-14 docnames c-pointer fs-12 fw-400 amt-label"  onClick={() => docPreview(item)}>{item.documentName}</span></div>
+                <div> <span className="icon md close c-pointer" onClick={() => docDelete(item)} /></div>
                  </div>
                 </>)}
              
