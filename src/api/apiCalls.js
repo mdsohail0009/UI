026df4bdbf4112ad930ffd6784f4ba935b3dfd49
Naresh Ch <naresh@tlvfintech.com>
2022-12-30 12:37:09 +0000

    Updated apiCalls.js

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -1,60 +1,225 @@
-import React, { Component } from 'react';
-import { Form, Input, Button, Checkbox } from 'antd';
+import { apiClient, ipRegistry ,bankClient} from "./";
+import { ApiControllers } from "./config";
+import counterpart from "counterpart";
+import CryptoJS from "crypto-js";
 
-const layout = {
-    labelCol: { span: 8 },
-    wrapperCol: { span: 16 },
+const getportfolio = (memID) => {
+	return apiClient.get(ApiControllers.wallets + `Crypto/${memID}`);
 };
-const tailLayout = {
-    wrapperCol: { offset: 8, span: 16 },
+const getCryptos = () => {
+	return apiClient.get(ApiControllers.buySell + "Coins");
 };
-class Login extends Component {
+const getMember = (customerid) => {
+	return apiClient.get(ApiControllers.customers + `${customerid}/App/Exchange`);
+};
+const sumsubacesstoken = (userid, flow) => {
+return apiClient.get(
+		"Sumsub/AccessToken1?applicantId=" + userid + "&levelName=" + flow
+	);
+};
+const sumsublivenessacesstoken = (userid, flow) => {
+	return apiClient.get(
+		"Sumsub/ExternalAccessToken?userId=" + userid + "&levelName=" + flow
+	);
+};
+const sumsubacesstokennew = (userid) => {
+	return apiClient.get("Sumsub/KYBAccessToken?applicantId=" + userid);
+};
+const updateKyc = (userid) => {
+	return apiClient.put(ApiControllers.customers + `${userid}/KYC`);
+};
+const trackEvent = (obj) => {
+	
+};
 
-    state = {
+const getIpRegistery = () => {
+	return ipRegistry.get("/?key=hb9lsmlhafyn1s1s");
+};
+const sellMemberCrypto = (memID) => {
+	return apiClient.get(ApiControllers.wallets + memID);
+};
+const convertLocalLang = (key) => {
+	return counterpart.translate(key);
+};
+const getIBANData = (ibannumber) => {
+	return apiClient.get(
+		ApiControllers.master + `GetIBANAccountDetails?ibanNumber=` + ibannumber
+	);
+};
 
-    }
-     onFinish = () => {
-      };
-    
-       onFinishFailed = () => {
-      };
-    render() {
+const getdshKpis = (customer_id) => {
+	return apiClient.get(ApiControllers.dashboard + `KPI/${customer_id}`);
+};
+const getdshcumulativePnl = (customer_id, days) => {
+	return apiClient.get(
+		ApiControllers.dashboard + `CumulativePNL/${customer_id}/${days}`
+	);
+};
+const getAssetNetwroth = (customer_id, days) => {
+	return apiClient.get(
+		ApiControllers.dashboard + `AssetsNetWorth/${customer_id}/${days}`
+	);
+};
+const getAssetAllowcation = (customer_id, days) => {
+	return apiClient.get(
+		ApiControllers.dashboard + `AssetAllocation/${customer_id}/${days}`
+	);
+};
+const getprofits = (customer_id, days) => {
+	return apiClient.get(ApiControllers.dashboard + `Profits/${customer_id}/${days}`);
+};
+const getdailypnl = (customer_id, days) => {
+	return apiClient.get(ApiControllers.dashboard + `DailyPNL/${customer_id}/${days}`);
+};
 
-        return <Form
-            {...layout}
-            name="basic"
-            initialValues={{ remember: true }}
-            onFinish={this.onFinish}
-            onFinishFailed={this.onFinishFailed}
-        >
-            <Form.Item
-                label="Username"
-                name="username"
-                rules={[{ required: true, message: 'Please input your username!' }]}
-            >
-                <Input />
-            </Form.Item>
+const getCode = (AccountId, isResendOTP) => {
+	return apiClient.get(
+		ApiControllers.master + `SendOTP/${AccountId}/${isResendOTP}`
+	);
+};
+const getVerification = (AccountId, code) => {
+	return apiClient.get(
+		ApiControllers.master + `OTPVerification/${AccountId}/${code}`
+	);
+};
 
-            <Form.Item
-                label="Password"
-                name="password"
-                rules={[{ required: true, message: 'Please input your password!' }]}
-            >
-                <Input.Password />
-            </Form.Item>
-
-            <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-                <Checkbox>Remember me</Checkbox>
-            </Form.Item>
-
-            <Form.Item {...tailLayout}>
-                <Button type="primary" htmlType="submit">
-                    Submit
-                 </Button>
-            </Form.Item>
-        </Form>
-
-    }
+const downloadKyc = (Customerid) => {
+	return apiClient.get(ApiControllers.customers + `DownloadFile/${Customerid}`);
+};
+const updateSecurity = (obj) => {
+	return apiClient.put(ApiControllers.master + "UpdateSecurity", obj);
+};
+const getCustomerBankDetails = (customerId)=>{
+    return bankClient.get(ApiControllers.bank + `GetAccountBalanceByCustomerId/${customerId}`);
 }
+const getAccountDetails=(customerId)=>{
+	return bankClient.get(ApiControllers.bank + `AccountDetails/${customerId}`)
+  }
 
-export default Login;
+const encryptValue = (msg, key) => {
+	msg = typeof msg == "string" ? msg : JSON.stringify(msg);
+	let salt = CryptoJS.lib.WordArray.random(128 / 8);
+
+	let key1 = CryptoJS.PBKDF2(key, salt, {
+		keySize: 256 / 32,
+		iterations: 10,
+	});
+
+	let iv = CryptoJS.lib.WordArray.random(128 / 8);
+
+	let encrypted = CryptoJS.AES.encrypt(msg, key1, {
+		iv: iv,
+		padding: CryptoJS.pad.Pkcs7,
+		mode: CryptoJS.mode.CBC,
+	});
+	return salt.toString() + iv.toString() + encrypted.toString();
+};
+const sendEmail = (AccountId, isResendOTP) => {
+	return apiClient.get(
+		ApiControllers.master + `SendEmailOTP/${AccountId}/${isResendOTP}`
+	);
+};
+
+const verifyEmail = (AccountId, code) => {
+	return apiClient.get(
+		ApiControllers.master + `EmailOTPVerification/${AccountId}/${code}`
+	);
+};
+const getAuthenticator = (Code, customerId) => {
+	return apiClient.get(
+		ApiControllers.master + `VerifyAuthenticator/${Code}/${customerId}`
+	);
+};
+const getVerificationFields = (customerId) => {
+	return apiClient.get(
+		ApiControllers.master + `Verificationfields/${customerId}`
+	);
+};
+const twofactor = (id) => {
+	return apiClient.get(ApiControllers.customers + `twofactor/${id}`);
+};
+
+const getInfoVal = (id, type) => {
+	return apiClient.get(
+		ApiControllers.deposit + `GetScoreChainInfo/${id}/${type}`
+	);
+};
+const getReferalDetails = (customerId) =>{
+	return apiClient.get(ApiControllers.partner + `getReferralDetails/customer/${customerId}`);
+}
+const getPayeeLu = (customerId,currency) => {
+    return apiClient.get(
+        ApiControllers.addressbook + `PayeeLu/${customerId}/${currency}`
+    );
+};
+const saveTransferData=(obj)=>{
+	return apiClient.post(ApiControllers.addressbook+'payee',obj)
+}
+const getRecipientData=(customerId,type,addressbookId)=>{
+	return apiClient.get(
+        ApiControllers.addressbook + `payee/Withdraw/Favourite/${addressbookId}/${customerId}/${type}`
+    );
+}
+const getPayeeCryptoLu = (customerId,currency) => {
+	return apiClient.get(
+		ApiControllers.addressbook + `PayeeCryptoLu/${customerId}/${currency}`
+	);
+};
+const getPayeeCrypto = (customerId,currency) => {
+	return apiClient.get(
+		ApiControllers.addressbook + `PayeeCrypto/${customerId}/${currency}`
+	);
+};
+const confirmCryptoTransaction = (obj) => {
+    return apiClient.post(ApiControllers.withdraw + `/Crypto/Confirm`, obj);
+}
+const convertUTCToLocalTime = (dateString) => {
+	let date = new Date(dateString);
+	const milliseconds = Date.UTC(
+		date.getFullYear(),
+		date.getMonth(),
+		date.getDate(),
+		date.getHours(),
+		date.getMinutes(),
+		date.getSeconds()
+	);
+	const localTime = new Date(milliseconds);
+	return ;
+};
+let apicalls = {
+	getportfolio,
+	getCryptos,
+	getMember,
+	sumsubacesstoken,
+	updateKyc,
+	sumsubacesstokennew,
+	sumsublivenessacesstoken,
+	 trackEvent,
+	sellMemberCrypto,
+	convertLocalLang,
+	getIBANData,
+	getdshKpis,
+	getdshcumulativePnl,
+	getAssetNetwroth,
+	getAssetAllowcation,
+	getprofits,
+	getdailypnl,
+	getCode,
+	getVerification,
+	getIpRegistery,
+	encryptValue,
+	downloadKyc,
+	updateSecurity,
+	sendEmail,
+	verifyEmail,
+	getAuthenticator,
+	getVerificationFields,
+	twofactor,
+	getInfoVal,
+	getReferalDetails,getPayeeLu,saveTransferData,getRecipientData,getCustomerBankDetails,getAccountDetails,
+	getPayeeCryptoLu,
+	getPayeeCrypto,
+	confirmCryptoTransaction,
+	convertUTCToLocalTime
+};
+export default apicalls;
