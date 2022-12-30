9f902a0d17a2e198ea9d53a86cb10da8fa91c553
Naresh <naresh@tlvfintech.com>
2022-12-30 18:02:43 +0530

    WithdrawCrypto ui to ux

--- src/components/withdraw.crypto.component/index.js
+++ src/components/withdraw.crypto.component/index.js
@@ -1,16 +1,12 @@
-import { Card, Typography, Empty,Image, Spin } from "antd";
+import { Typography,  Spin } from "antd";
 import { useEffect } from "react";
-import Translate from "react-translate-component";
-import { fetchWithDrawWallets, handleSendFetch, setSelectedWithDrawWallet, setStep, setSubTitle } from "../../reducers/sendreceiveReducer";
-import ConnectStateProps from "../../utils/state.connect";
-import Currency from '../shared/number.formate';
+import { fetchWithDrawWallets, handleSendFetch, setSelectedWithDrawWallet, setStep} from "../../reducers/sendreceiveReducer";
 import Loader from '../../Shared/loader';
-import apicalls from "../../api/apiCalls";
 import CryptoList from '../shared/cryptolist';
 import { getFeaturePermissionsByKeyName } from '../shared/permissions/permissionService'
 import { connect } from "react-redux";
 
-const { Paragraph, Text, Title } = Typography;
+const { Title } = Typography;
 const WithdrawCrypto = ({ dispatch, userProfile, sendReceive, props, changeStep }) => {
     useEffect(() => {
         loadData();
@@ -29,33 +25,13 @@ const WithdrawCrypto = ({ dispatch, userProfile, sendReceive, props, changeStep 
    const handleCoinSelection = (wallet) => {
     dispatch(setSelectedWithDrawWallet(wallet)); 
     dispatch(setStep('withdraw_crypto_selected'))
-   // changeStep('withdraw_crpto_summary');
     }
     return <>
         <div className="mt-8">
                     <div className='label-style'>Select An Asset To Send From Your Crypto Wallet</div>
                 </div>
         <div className="dep-withdraw auto-scroll">
-            {/* {wallets?.data.length ? <>{wallets?.data?.map((wallet, indx) => <Card key={indx} className="crypto-card mb-16 c-pointer" bordered={false} onClick={() => { dispatch(setSelectedWithDrawWallet(wallet)); dispatch(setStep('withdraw_crypto_selected')) }} >
-                
-                <div className="crypto-details d-flex">
-                    <div>
-                    <span className="d-flex align-center mb-4">
-                    <Image preview={false} src={wallet.impageWhitePath}/>
-                    <div className="crypto-percent">{wallet.percentage}<sup className="percent">%</sup></div>
-                    
-                </span> 
-                
-                <Text className="fs-24 text-purewhite ml-4 mt-8">{wallet.coinFullName}</Text>
-                    </div>
-                    <div>
-                    <div className="crypto-amount">
-                        <Currency defaultValue={wallet.coinBalance} prefix={""} type={"text"} suffixText={wallet.coin} />
-                        <Currency defaultValue={wallet.coinValueinNativeCurrency} prefix={"$"} type={"text"} />
-                    </div>
-                </div></div>
-            </Card>)}</> : <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description={apicalls.convertLocalLang('No_data')} />} */}
-             {/* <CryptoList  isLoading={wallets?.loading} showSearch={true} coinList={wallets?.data} coinType={"swap"} onCoinSelected={(wallet) => handleCoinSelection(wallet)} /> */}
+           
             <Spin spinning={wallets?.loading}><CryptoList showSearch={true} titleField={'coin'} iconField={'coin'} showValues={true} coinList={wallets?.loading ? [] : wallets?.data} isLoading={wallets?.loading} coinType={"swap"} onCoinSelected={(wallet) => handleCoinSelection(wallet)}/></Spin>
         </div>
 
