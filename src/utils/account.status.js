44c14f7e55a0c7ce35546ca64f8deb301d998bdd
Naresh <naresh@tlvfintech.com>
2022-12-31 16:28:13 +0530

    customer state change issue

--- src/utils/account.status.js
+++ src/utils/account.status.js
@@ -3,7 +3,7 @@ import pending from '../assets/images/pending1.png'
 class AccountStatus extends Component {
     render() {
         return <div className='sumSub-confirm text-white text-center'><img src={pending} className="confirm-icon" alt={"success"} /><br />
-            <span className='sumSub-review'>Your account approval is in progress state</span>
+            <span className='sumSub-review'>{this.props.customerState?.customerState==="Under Review"?"Your account is under review":"Your account approval is in progress state"}</span>
             <p className='p-0' style={{ wordBreak: 'break-all' }}> Please contact administrator</p></div>
     }
 
