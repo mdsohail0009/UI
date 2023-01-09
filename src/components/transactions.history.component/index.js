31d1c55d4259947871ada5fff49efc65853d771e
naresh <naresh@tlvfintech.com>
2023-01-09 20:32:58 +0530

    hashchanges

--- src/components/transactions.history.component/index.js
+++ src/components/transactions.history.component/index.js
@@ -154,9 +154,9 @@ class TransactionsHistory extends Component {
       field: "hash", title: "Hash", filter: true, width: 180,
       customCell: (props) => (
         <td>
-         <Tooltip title={props.dataItem.hashId}>
-         {(props.dataItem.hashId && <a className="gridLink c-pointer"  onClick={() => window.open(`${props.dataItem?.explorer}${props.dataItem?.hashId}`, '_blank')}>
-          <span className="gridLink c-pointer">{props?.dataItem?.hashId?.slice(0, 4) +"......." +props?.dataItem?.hashId?.slice(-4)}</span></a>) || " "}
+         <Tooltip title={props.dataItem.hash}>
+         {(props.dataItem.hash && <a className="gridLink c-pointer"  onClick={() => window.open(`${props.dataItem?.explorer}${props.dataItem?.hash}`, '_blank')}>
+          <span className="gridLink c-pointer">{props?.dataItem?.hash?.slice(0, 4) +"......." +props?.dataItem?.hash?.slice(-4)}</span></a>) || " "}
        </Tooltip>
         </td>
       ),
