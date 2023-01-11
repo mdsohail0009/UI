deccf1c292ce6d2012a235b9ea16ffa33a9f089c
naresh <naresh@tlvfintech.com>
2023-01-11 11:14:36 +0530

    declaration changes

--- src/components/addressbook.component/addressCommonCom.js
+++ src/components/addressbook.component/addressCommonCom.js
@@ -514,7 +514,7 @@ const AddressCommonCom = (props) => {
     return <div className="custom-declaraton"> <div className="success-pop text-center declaration-content">
       <Image width={80} preview={false} src={success} className="confirm-icon" />
       <Title level={2} className="success-title">Declaration form sent successfully</Title>
-                <Text className="successsubtext">{`Declaration form has been sent to ${this.props.userProfile?.email}. 
+                <Text className="successsubtext">{`Declaration form has been sent to ${props.userProfile?.email}. 
                 Please review and sign the document in your email to whitelist your address.
                 Please note that your withdrawal will only be processed once the address has been approved by compliance. `}</Text>
     </div></div>
