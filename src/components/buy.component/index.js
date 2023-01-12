cf464d7d730c627d75bdaa57b13da8f2fa5cb208
lakshmi durga <rao@tlvfintech.com>
2023-01-12 17:20:25 +0530

    designchanges

--- src/components/buy.component/index.js
+++ src/components/buy.component/index.js
@@ -123,13 +123,11 @@ class BuySell extends Component {
                 
                 }
                 {((!this.props.isTabKey && !this.props.buySell?.sellHeader) || this.props.buySell?.selectedSellCoin)&& <div className="text-center sell-title-styels">
-                    {!["step1","success"].includes(this.props.buySell.stepcode) && <Image preview={false} src={this.props.sellData?.coinWallet?.impagePath} />
+                 {!["step1","success"].includes(this.props.buySell.stepcode) &&<Image preview={false} src={this.props.sellData?.coinWallet?.impagePath} />                     }
                     <Translate 
                     with={{ coin: this.props.sellData?.coinWallet?.walletCode || this.props.sellData?.coinWallet?.coin }}
                      className="drawer-maintitle buy-sellprocess" content={this.props.buySell.stepTitles[config[this.props.buySell.stepcode]]} component={Paragraph} />
-                     {!["step1","success"].includes(this.props.buySell.stepcode) &&
-                     <Image preview={false} src={this.props.sellData?.coinWallet?.impagePath} />
-                     }
+                   
                      
                 </div>}</>
             {this.renderContent()}
