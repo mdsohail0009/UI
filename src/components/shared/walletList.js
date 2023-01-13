b38d4e61e81418948306f7ed5037de3551d89372
Naresh <naresh@tlvfintech.com>
2023-01-13 11:08:16 +0530

    sell default selection

--- src/components/shared/walletList.js
+++ src/components/shared/walletList.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import { Select, List,Empty, Image } from 'antd';
+import { Select, List,Empty } from 'antd';
 import { setStep } from '../../reducers/buysellReducer';
 import { Link ,} from "react-router-dom";
 import Translate from "react-translate-component";
 import { connect } from 'react-redux';
 import { fetchMemberFiat } from '../../reducers/buyReducer';
 import NumberFormat from 'react-number-format'
-import apicalls from '../../api/apiCalls';
 
-const { Option } = Select;
+
 class WalletList extends Component {
     state = {
         isArrow: true,
@@ -20,10 +19,11 @@ class WalletList extends Component {
         }
     }
     componentDidMount() {
+        const {buyInfo}=this.props.buyInfo;
         this.props.getFiat(this.props.member?.id);
-        if( this.props.buyInfo?.memberFiat?.data[0]?.id){
-            if (this.props.onWalletSelect) { this.props.onWalletSelect(this.props.buyInfo?.memberFiat?.data[0]?.id) }
-            this.setState({ ...this.state, selectedvalue:this.props.buyInfo?.memberFiat?.data[0]?.id });
+        if(buyInfo?.memberFiat?.data[0]?.id){
+            if (this.props.onWalletSelect) { this.props.onWalletSelect(buyInfo?.memberFiat?.data[0]?.id ) }
+            this.setState({ ...this.state, selectedvalue:buyInfo?.memberFiat?.data[0]?.id });
         }
 
     }
@@ -31,25 +31,10 @@ class WalletList extends Component {
         return (<>
             {this.props.buyInfo.memberFiat &&
                 <form className="form" id="withdrawCurrency">
-                    {/* <Select    getPopupContainer={() => document.getElementById('withdrawCurrency')} dropdownClassName="select-drpdwn" loading={this.props?.buyInfo?.memberFiat?.loading} placeholder={this.props.placeholder || apicalls.convertLocalLang("selectWallet")} className="cust-input" style={{ width: '100%' }} bordered={false} showArrow={true}
-                        value={this.props.selectedvalue ? this.props.selectedvalue : this.state.selectedvalue} 
-                        onChange={(e) => {
-                            
-                            if (this.props.onWalletSelect) { this.props.onWalletSelect(e) }
-                            this.setState({ ...this.state, selectedvalue: e });
-                           // this.props.sendCurrency(e)      
-                        }}>
-                        {this.props.buyInfo.memberFiat?.data?.map((item, idx) =>
-                            <Option key={idx} className="fw-400" value={item[this.props.valueFeild || 'id']}>{item.currencyCode}
-                                {!this.props.hideBalance && <NumberFormat value={item.avilable} displayType={'text'} thousandSeparator={true} prefix={this.state.symbols[item.currencyCode]} renderText={(value, props) => <span {...props}> Balance: {value}</span>} />}
-                            </Option>
-                        )}
-                    </Select> */}
                     <List
                     itemLayout="horizontal"
                     dataSource={this.props.buyInfo.memberFiat?.data}
                     className="crypto-list auto-scroll wallet-list selection-currency-list"
-                    // loading={this.state.fiatWalletsLoading}
                     locale={{
                         emptyText: <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description={
                             <Translate content="No_data" />
@@ -58,13 +43,13 @@ class WalletList extends Component {
                     renderItem={item => (
 
                         <List.Item
-                        className={this.props.buyInfo?.selectedWallet?.currencyCode===item?.currencyCode?"select":""}
+                        className={(this.props.buyInfo?.selectedWallet?.currencyCode ===item?.currencyCode || this.props.buyInfo?.selectedWallet?.toWalletCode ===item?.currencyCode)?"select":""}
                          onClick={() => {
                             
                             if (this.props.onWalletSelect) { this.props.onWalletSelect(item.id) }
-                            this.setState({ ...this.state, selectedvalue: item.id });
-                           // this.props.sendCurrency(e)      
+                            this.setState({ ...this.state, selectedvalue: item.id });  
                         }}>
+                          
                             <Link>
                                 <List.Item.Meta className='drawer-coin'
                                     avatar={<div className='crypto-bg'><span className='crypto-icon c-pointer ETH'></span></div>}
