3f5bef7a7d84cf4644eb377c88d8218ee8d80211
Naresh <naresh@tlvfintech.com>
2023-02-11 15:12:02 +0530

    case redirection changes changes

--- src/components/shared/doc.notices.js
+++ src/components/shared/doc.notices.js
@@ -9,10 +9,20 @@ const DocNotices = (props) => {
     useEffect(() => {
         props.dispatch(fetchNotices(props.userProfile.id))
     }, []);// eslint-disable-line react-hooks/exhaustive-deps
+
+ const goToDetails=(data)=>{
+        if(data.isReqforDoc)
+        {
+            props.history?.push("/cases?id=" + data.typeId)
+        }else{
+            props.history.push(`/addressbook`)
+        }
+       
+    }
     return <React.Fragment style={{ top: 10 }}>
         <div className="main-container">
             {props?.dashboard?.notices?.data.length !== 0 && <Alert style={{ padding: 16 }} type="info" message={"Please submit the listed documents to proceed"} showIcon />}
-            {props?.dashboard?.notices.loading === true ? <div className="text-center p-24"><Spin size="default" /></div> : <>{props?.dashboard?.notices?.data?.map(item => <Alert style={{ cursor: "pointer" }} type="error" showIcon onClick={() => props.history?.push("/cases?id=" + item.typeId)} message={item.title} description="Our Compliance Team is requesting documents in line with your recent transaction, Please click View Details. Thank you for your patience." />)}</>}
+            {props?.dashboard?.notices.loading === true ? <div className="text-center p-24"><Spin size="default" /></div> : <>{props?.dashboard?.notices?.data?.map(item => <Alert style={{ cursor: "pointer" }} type="error" showIcon onClick={() =>goToDetails(item) } message={item.title} description="Our Compliance Team is requesting documents in line with your recent transaction, Please click View Details. Thank you for your patience." />)}</>}
             {(props?.dashboard?.notices?.data == null || props?.dashboard?.notices?.data.length === 0) && !props?.dashboard?.notices.loading && < Result status="404"
                 title={<h4 className="text-white">No request documents, Please click below button to go to dashboard.</h4>}
                 extra={<Button className="pop-btn" onClick={() => { props.dispatch(updateDocRequest(false)); props.history.push("/cockpit") }}>Go to Dashboard</Button>}
