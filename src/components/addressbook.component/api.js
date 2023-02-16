10a212710c557954b8b04932b843de07493bfb3c
Naresh Ch <nareshchebrolu@yellowblock.net>
2023-02-16 14:38:40 +0530

    msg

--- src/components/addressbook.component/api.js
+++ src/components/addressbook.component/api.js
@@ -134,17 +134,21 @@ const networkLu = (coinName) => {
     );
 };
 const isErrorDispaly = (objValue) => {
-    if (objValue.data && typeof objValue.data === "string") {
-      return objValue.data;
-    } else if (
-      objValue.originalError &&
-      typeof objValue.originalError.message === "string"
-    ) {
-      return objValue.originalError.message;
-    } else {
-      return "Something went wrong please try again!";
-    }
-  };
+	if ((objValue.status >= 400 && objValue.status < 500) && objValue.status != 401) {
+		return "Something went wrong please try again!";
+	} else {
+		if (objValue.data && typeof objValue.data === "string") {
+			return objValue.data;
+		} else if (
+			objValue.originalError &&
+			typeof objValue.originalError.message === "string"
+		) {
+			return objValue.originalError.message;
+		} else {
+			return "Something went wrong please try again!";
+		}
+	}
+};
 export {
 	emailCheck,
 	getPayeeLu,
