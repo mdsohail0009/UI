1c4f4b549231ee115cf3b256b510728b419fb46c
Naresh Ch <nareshchebrolu@yellowblock.net>
2023-02-17 15:05:01 +0530

    addressboom documents

--- src/components/addressbook.component/addressCryptoView.js
+++ src/components/addressbook.component/addressCryptoView.js
@@ -3,9 +3,12 @@ import { Row, Col, Typography, Button, Modal, Tooltip } from "antd";
 import Loader from "../../Shared/loader";
 import { getFileURL, getCryptoData } from "./api";
 import { connect } from "react-redux";
-import FilePreviewer from "react-file-previewer";
+//import FilePreviewer from "react-file-previewer";
 import { bytesToSize } from "../../utils/service";
 import { addressTabUpdate} from "../../reducers/addressBookReducer";
+import DocumentPreview from '../../Shared/docPreview'
+
+
 const { Title, Text } = Typography;
 const EllipsisMiddle = ({ suffixCount, children }) => {
 	const start = children?.slice(0, children.length - suffixCount).trim();
@@ -22,10 +25,11 @@ const EllipsisMiddle = ({ suffixCount, children }) => {
 const AddressCryptoView = (props) => {
 	const [loading, setIsLoading] = useState(false);
 	const [cryptoAddress, setCryptoAddress] = useState({});
-	const [previewPath, setPreviewPath] = useState(null);
-	const [previewModal, setPreviewModal] = useState(false);
-	const [mimeType,setMimeType]=useState(false);
-
+	//const [previewPath, setPreviewPath] = useState(null);
+	//const [previewModal, setPreviewModal] = useState(false);
+	//const [mimeType,setMimeType]=useState(false);
+	const [docPreviewDetails, setDocPreviewDetails] = useState(null)
+	const [docPreviewModal, setDocPreviewModal] = useState(false)
 
 	useEffect(() => {
 		loadDataAddress();
@@ -42,75 +46,87 @@ const AddressCryptoView = (props) => {
 		props?.history?.push("/addressbook");
 		props?.dispatch(addressTabUpdate(false));
 	};
-	const docPreview = async (file) => {
-		const mimeType = {
-		"pdf": "pdf",
-		"jpg": "jpg",
-		"jpeg": "jpeg",
-		"png": "png",
-		 "PDF": "PDF",
-		"PNG": "PNG",
-		 "JPEG": "JPEG" };
-		let res = await getFileURL({ url: file.path });
-		if (res.ok) {
-			setPreviewModal(true);
-			setPreviewPath(res.data);
-		const fileName=file.fileName.split(".")
-			if(mimeType[fileName[1]])
-			{
-				setMimeType(true);
-			}
+	// const docPreview = async (file) => {
+	// 	const mimeType = {
+	// 	"pdf": "pdf",
+	// 	"jpg": "jpg",
+	// 	"jpeg": "jpeg",
+	// 	"png": "png",
+	// 	 "PDF": "PDF",
+	// 	"PNG": "PNG",
+	// 	 "JPEG": "JPEG" };
+	// 	let res = await getFileURL({ url: file.path });
+	// 	if (res.ok) {
+	// 		setPreviewModal(true);
+	// 		setPreviewPath(res.data);
+	// 	const fileName=file.fileName.split(".")
+	// 		if(mimeType[fileName[1]])
+	// 		{
+	// 			setMimeType(true);
+	// 		}
 			
-		}
-	};
-	const filePreviewPath = () => {
-		return previewPath;
+	// 	}
+	// };
+	// const filePreviewPath = () => {
+	// 	return previewPath;
 
-	};
+	// };
+
+	const docPreviewOpen = (data) => {
+		debugger
+		setDocPreviewModal(true)
+		setDocPreviewDetails({ id: data.id, fileName: data.fileName })
+	  }
+	  console.log(docPreviewDetails)
+
+	const docPreviewClose = () => {
+		setDocPreviewModal(false)
+		setDocPreviewDetails(null)
+	  }
 	
-	const filePreviewModal = (
-		<Modal
-			className="documentmodal-width"
-			destroyOnClose={true}
-			title="Preview"
-			width={1000}
-			visible={previewModal}
-			closeIcon={
-				<Tooltip title="Close">
-					<span
-						className="icon md close-white c-pointer"
-						onClick={() => setPreviewModal(false)}
-					/>
-				</Tooltip>
-			}
-			footer={
-				<>
-					<div className="cust-pop-up-btn crypto-pop">
+	// const filePreviewModal = (
+	// 	<Modal
+	// 		className="documentmodal-width"
+	// 		destroyOnClose={true}
+	// 		title="Preview"
+	// 		width={1000}
+	// 		visible={previewModal}
+	// 		closeIcon={
+	// 			<Tooltip title="Close">
+	// 				<span
+	// 					className="icon md close-white c-pointer"
+	// 					onClick={() => setPreviewModal(false)}
+	// 				/>
+	// 			</Tooltip>
+	// 		}
+	// 		footer={
+	// 			<>
+	// 				<div className="cust-pop-up-btn crypto-pop">
 					
-					<Button
-						className="cust-cancel-btn cust-cancel-btn pay-cust-btn detail-popbtn paynow-btn-ml"
+	// 				<Button
+	// 					className="cust-cancel-btn cust-cancel-btn pay-cust-btn detail-popbtn paynow-btn-ml"
 					
-						onClick={() => setPreviewModal(false)}>
-						Close
-					</Button>
-					<Button
-						className="primary-btn pop-btn detail-popbtn"
+	// 					onClick={() => setPreviewModal(false)}>
+	// 					Close
+	// 				</Button>
+	// 				<Button
+	// 					className="primary-btn pop-btn detail-popbtn"
 					
-						onClick={() => window.open(previewPath, "_blank")}>
-						Download
-					</Button>
-					</div>
-				</>
-			}>
-			<FilePreviewer
-				hideControls={true}
-				file={{
-					url: previewPath ? filePreviewPath() : null,
-					mimeType: previewPath?.includes(".pdf") ? "application/pdf" : "",
-				}}
-			/>
-		</Modal>
-	);
+	// 					onClick={() => window.open(previewPath, "_blank")}>
+	// 					Download
+	// 				</Button>
+	// 				</div>
+	// 			</>
+	// 		}>
+	// 		<FilePreviewer
+	// 			hideControls={true}
+	// 			file={{
+	// 				url: previewPath ? filePreviewPath() : null,
+	// 				mimeType: previewPath?.includes(".pdf") ? "application/pdf" : "",
+	// 			}}
+	// 		/>
+	// 	</Modal>
+	// );
 
 	return (
 		<>
@@ -228,7 +244,9 @@ const AddressCryptoView = (props) => {
 															/>
 															<div
 																className="docdetails c-pointer"
-																onClick={() => docPreview(file)}>
+																//onClick={() => docPreview(file)}
+																onClick={() => docPreviewOpen(file)}
+																>
 																{file.name !== null ? (
 																	<EllipsisMiddle suffixCount={4}>
 																		{file.fileName}
@@ -263,7 +281,13 @@ const AddressCryptoView = (props) => {
 					)}
 				</div>
 			</div>
-			{filePreviewModal}
+			{/* {filePreviewModal} */}
+			{docPreviewModal &&
+      <DocumentPreview
+        previewModal={docPreviewModal}
+        handleCancle={docPreviewClose}
+        upLoadResponse={docPreviewDetails}
+      />}
 		</>
 	);
 };
