010ceaad84bcaa977e542470a1bbf0ef7756878e
Naresh <naresh@tlvfintech.com>
2023-03-16 16:18:10 +0530

    config changes

--- src/utils/account.status.js
+++ src/utils/account.status.js
@@ -5,7 +5,7 @@ class AccountStatus extends Component {
         return <div className='sumSub-confirm text-white text-center'><img src={pending} className="confirm-icon" alt={"success"} /><br />
             <span className='sumSub-review'>{this.props.customerState?.customerState==="Under Review"?"Your account is under review state":<>Your account approval is in progress.
             <p className="approve-email">
-             Contact our customer success at  <a href = "mailto: process.env.REACT_APP_ONBOARDING">{process.env.REACT_APP_ONBOARDING}</a> for more information.</p></>}</span>
+             Contact our customer success at  <a href = {`mailto: ${process.env.REACT_APP_ONBOARDING}`}>{process.env.REACT_APP_ONBOARDING}</a> for more information.</p></>}</span>
            
             </div>
     }
