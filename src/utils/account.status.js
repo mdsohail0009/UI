078bdb6de9ca0c61edf09a4de129dfd0dbf74032
Naresh <naresh@tlvfintech.com>
2023-04-12 21:12:47 +0530

    changes

--- src/utils/account.status.js
+++ src/utils/account.status.js
@@ -4,16 +4,22 @@ import Acsacnner from '../../src/assets/images/scannerac.jpg';
 import AcsacnnerIo from '../../src/assets/images/scannerIo.png'
 
 class AccountStatus extends Component {
-    render() {
-        return<div className='sumSub-confirm text-white d-flex align-center'><div className='text-center cust-sumsub-font'><img src={pending} className="confirm-icon" alt={"success"} />   <div className='sumSub-review'>{this.props.customerState?.customerState==="Under Review"?"Your account is under review":<>Your account approval is in progress.
-            <p className="approve-email">
-             Contact our Customer success at  <a href = {`mailto: ${process.env.REACT_APP_ONBOARDING}`}>{process.env.REACT_APP_ONBOARDING}</a> for more information.</p></>}</div>
-             {this.props.customerState?.customerState==="Under Review" 
-            
-             }</div>
-            <div className='text-white cust-sumsub-font text-center'>
-            <div className='sumSub-review sumsub-mb'>{this.props.customerState?.customerState==="Under Review"?"Please complete your application using the QR code or button below:":<>Your account approval is in progress.
-            </>}</div>
+  render() {
+    return <>
+      {this.props.customerState?.customerState == "Under Review" ? (
+        <div className='sumSub-confirm text-white d-flex align-center'>
+          <div className='text-center cust-sumsub-font'>
+            <img src={pending} className="confirm-icon" alt={"success"} />
+            <div className='sumSub-review'>
+              {this.props.customerState?.customerState === "Under Review" ? "Your account is under review" : <>Your account approval is in progress.
+                <p className="approve-email">
+                  Contact our Customer success at <a href={`mailto: ${process.env.REACT_APP_ONBOARDING}`}>{process.env.REACT_APP_ONBOARDING}</a> for more information.</p></>}
+            </div>
+            {this.props.customerState?.customerState ==="Under Review"
+            }</div>
+          <div className='text-white cust-sumsub-font text-center'>
+            <div className='sumSub-review sumsub-mb'>{this.props.customerState?.customerState === "Under Review" ? "Please complete your application using the QR code or button below:" : <>Your account approval is in progress.
+            </>} </div>
             {process.env.REACT_APP_ACCOUNT_STATUS_SCANNER_IS==="true" &&
             <img src={AcsacnnerIo} width="150" />}
             {process.env.REACT_APP_ACCOUNT_STATUS_SCANNER_IS==="false" &&
@@ -31,8 +37,19 @@ class AccountStatus extends Component {
             </div>
             </div>
           
-    }
+  
+      ) : (
+        <div className='sumSub-confirm text-white text-center'><img src={pending} className="confirm-icon" alt={"success"} /><br />
+          <span className='sumSub-review'>{this.props.customerState?.customerState === "Under Review" ? "Your account is under review state" : <>Your account approval is in progress.
+            <p className="approve-email">
+              Contact our Customer success at <a href={`mailto: ${process.env.REACT_APP_ONBOARDING}`}>{process.env.REACT_APP_ONBOARDING}</a> for more information.</p></>}</span>
+          {this.props.customerState?.customerState === "Under Review" && <p className='approve-email' style={{ wordBreak: 'break-all' }}> Please contact administrator</p>}
+        </div>
+      )}
+    </>
+
+  }
 
 }
 
-export default AccountStatus;   
+export default AccountStatus; 
