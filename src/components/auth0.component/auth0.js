f0f43820ca1c054f1df7222118a1ed05e55c160d
saranya <saranya.ziraff@gmail.com>
2023-04-19 19:40:40 +0530

    auth0 designs

--- src/components/auth0.component/auth0.js
+++ src/components/auth0.component/auth0.js
@@ -1,23 +1,96 @@
-import {useState} from "react";
-import {Typography,List,Button,Image,Dropdown,Space,Menu,} from "antd";
-import {Link } from "react-router-dom";
-import { Steps } from 'antd';
+import React, { useState } from 'react';
+import { Steps, Button, Checkbox,Row,Col,Form,Select } from 'antd';
+const { Option } = Select;
+const { Step } = Steps;
 
-const {Step}=Steps;
-
-function Auth0() {
-    const [current, setCurrent] = useState(1);
-    return(
-        <div className="main-container">
-            <Steps current={current} percent={50} onChange={(c) => {
-                setCurrent(c);
-            }}>
-                <Step title='Finished'></Step>
-                <Step title='In Progress'></Step>
-                <Step title='Waiting'></Step>
-            </Steps>
+const steps = [
+    {
+      title: 'Step 1',
+      content: (
+        // <Checkbox.Group>
+        //   <Checkbox value="A" className='text-white'>
+        //     <input type="checkbox" id="agree-check1" />
+		// 	<span for="agree-check"  className="c-pointer"/> 
+        //     Personal Account
+        //   </Checkbox>
+        //   <Checkbox value="B" className='text-white'>
+        //     <input type="checkbox" id="agree-check1" />
+		// 	<span for="agree-check"  className="c-pointer"/> 
+        //     Bussiness Account</Checkbox>
+        // </Checkbox.Group>
+      ),
+    },
+    {
+      title: 'Step 2',
+      content: (
+       <>
+       <Row>
+        <Col xs={24} md={24} lg={12} xl={12} xxl={12}>
+                      <Form.Item className=" mb-8 px-4 text-white-50 custom-forminput custom-label pt-8 sc-error"
+                          name="network"
+                          label="Network (Any coins on the selected network will be whitelisted)"
+                          rules={[
+                              {
+                                  required: true,
+                                  message: "Is required",
+                              },
+                          ]}
+                      >
+                          <Select
+                              className="cust-input Approved"
+                              maxLength={100}
+                              placeholder="Select Network"
+                              optionFilterProp="children"
+                          >
+                            <Option>asfsd</Option>
+                            <Option>asfsd</Option>
+                            <Option>asfsd</Option>
+                          </Select>
+                      </Form.Item>
+        </Col>
+       </Row>
+       </>
+      ),
+    },
+    
+  ];
+  const Auth0 = () => {
+    const [current, setCurrent] = useState(0);
+  
+    const next = () => {
+      setCurrent(current + 1);
+    };
+  
+    const prev = () => {
+      setCurrent(current - 1);
+    };
+  
+    return (
+      <div className='register-blockwid form-block'>
+        <Steps current={current}>
+          {steps.map(item => (
+            <Step key={item.title} title={item.title} />
+          ))}
+        </Steps>
+        <div>{steps[current].content}</div>
+        <div>
+          {current < steps.length - 1 && (
+            <Button type="primary" onClick={() => next()}>
+              Next
+            </Button>
+          )}
+          {current === steps.length - 1 && (
+            <Button type="primary" onClick={() => console.log('Processing complete!')}>
+              Next
+            </Button>
+          )}
+          {current > 0 && (
+            <Button style={{ margin: '0 8px' }} onClick={() => prev()}>
+              Back
+            </Button>
+          )}
         </div>
+      </div>
     );
-}
-
-export default Auth0;
+  };
+    export default Auth0;
