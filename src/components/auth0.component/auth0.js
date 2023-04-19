f0ee3ae418bb2e53bc8a3c614f3ef721525768c7
saranya <saranya.ziraff@gmail.com>
2023-04-19 20:10:12 +0530

    auth form

--- src/components/auth0.component/auth0.js
+++ src/components/auth0.component/auth0.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Steps, Button, Checkbox,Row,Col,Form,Select } from 'antd';
+import { Steps, Button, Checkbox,Row,Col,Form,Select,Input } from 'antd';
 const { Option } = Select;
 const { Step } = Steps;
 
@@ -19,7 +19,10 @@ const steps = [
         //     Bussiness Account</Checkbox>
         // </Checkbox.Group>
         <>
-        
+            <div className='account-content'>
+                <div className='text-personal account-content'>Personal Account</div>
+                <div className='text-personal account-content'>Bussiness Account</div>
+            </div>
         </>
       ),
     },
@@ -27,8 +30,8 @@ const steps = [
       title: 'Step 2',
       content: (
        <>
-       <Row>
-        <Col xs={24} md={24} lg={12} xl={12} xxl={12}>
+       <Row className='formfields-block'>
+        {/* <Col xs={24} md={24} lg={12} xl={12} xxl={12}>
                       <Form.Item className=" mb-8 px-4 text-white-50 custom-forminput custom-label pt-8 sc-error"
                           name="network"
                           label="Network (Any coins on the selected network will be whitelisted)"
@@ -37,8 +40,7 @@ const steps = [
                                   required: true,
                                   message: "Is required",
                               },
-                          ]}
-                      >
+                          ]}>
                           <Select
                               className="cust-input Approved"
                               maxLength={100}
@@ -50,6 +52,226 @@ const steps = [
                             <Option>asfsd</Option>
                           </Select>
                       </Form.Item>
+        </Col> */}
+        <Col xs={24} md={24} lg={12} xl={12} xxl={12}>
+        <Form.Item
+             className=" mb-8 px-4 text-white-50 custom-forminput custom-label pt-8 sc-error"
+              name="Legal Business Name"
+              label="Legal Business Name"
+              required
+              rules={[
+                {              
+                  required: true,
+                  message: "Is required",
+                },
+               
+                
+              ]}
+            >
+              <Input
+                className="cust-input form-disable"
+                maxLength={100}
+                placeholder="Wallet Address"
+              />
+            </Form.Item>
+        </Col>
+        <Col xs={24} md={24} lg={12} xl={12} xxl={12}>
+        <Form.Item
+             className=" mb-8 px-4 text-white-50 custom-forminput custom-label pt-8 sc-error"
+              name="Email"
+              label="Email"
+              required
+              rules={[
+                {              
+                  required: true,
+                  message: "Is required",
+                },
+               
+                
+              ]}
+            >
+              <Input
+                className="cust-input form-disable"
+                maxLength={100}
+                placeholder="Wallet Address"
+              />
+            </Form.Item>
+        </Col>
+        <Col xs={24} md={24} lg={12} xl={12} xxl={12}>
+        <Form.Item
+             className=" mb-8 px-4 text-white-50 custom-forminput custom-label pt-8 sc-error"
+              name="Phone"
+              label="Phone"
+              required
+              rules={[
+                {              
+                  required: true,
+                  message: "Is required",
+                },
+               
+                
+              ]}
+            >
+              <Input
+                className="cust-input form-disable"
+                maxLength={100}
+                placeholder="Wallet Address"
+              />
+            </Form.Item>
+        </Col>
+        <Col xs={24} md={24} lg={12} xl={12} xxl={12}>
+        <Form.Item
+             className=" mb-8 px-4 text-white-50 custom-forminput custom-label pt-8 sc-error"
+              name="Country Of Business"
+              label="Country Of Business"
+              required
+              rules={[
+                {              
+                  required: true,
+                  message: "Is required",
+                },
+               
+                
+              ]}
+            >
+              <Input
+                className="cust-input form-disable"
+                maxLength={100}
+                placeholder="Wallet Address"
+              />
+            </Form.Item>
+        </Col>
+        <Col xs={24} md={24} lg={12} xl={12} xxl={12}>
+        <Form.Item
+             className=" mb-8 px-4 text-white-50 custom-forminput custom-label pt-8 sc-error"
+              name="Username"
+              label="Username"
+              required
+              rules={[
+                {              
+                  required: true,
+                  message: "Is required",
+                },
+               
+                
+              ]}
+            >
+              <Input
+                className="cust-input form-disable"
+                maxLength={100}
+                placeholder="Wallet Address"
+              />
+            </Form.Item>
+        </Col>
+        <Col xs={24} md={24} lg={12} xl={12} xxl={12}>
+        <Form.Item
+             className=" mb-8 px-4 text-white-50 custom-forminput custom-label pt-8 sc-error"
+              name="Password"
+              label="Password"
+              required
+              rules={[
+                {              
+                  required: true,
+                  message: "Is required",
+                },
+               
+                
+              ]}
+            >
+              <Input
+                className="cust-input form-disable"
+                maxLength={100}
+                placeholder="Wallet Address"
+              />
+            </Form.Item>
+        </Col>
+        <Col xs={24} md={24} lg={12} xl={12} xxl={12}>
+        <Form.Item
+             className=" mb-8 px-4 text-white-50 custom-forminput custom-label pt-8 sc-error"
+              name="Referral Code"
+              label="Referral Code"
+              required
+              rules={[
+                {              
+                  required: true,
+                  message: "Is required",
+                },
+               
+                
+              ]}
+            >
+              <Input
+                className="cust-input form-disable"
+                maxLength={100}
+                placeholder="Wallet Address"
+              />
+            </Form.Item>
+        </Col>
+        <Col xs={24} md={24} lg={12} xl={12} xxl={12}>
+        <Form.Item
+             className=" mb-8 px-4 text-white-50 custom-forminput custom-label pt-8 sc-error"
+              name="First Name"
+              label="First Name"
+              required
+              rules={[
+                {              
+                  required: true,
+                  message: "Is required",
+                },
+               
+                
+              ]}
+            >
+              <Input
+                className="cust-input"
+                maxLength={100}
+                placeholder="Wallet Address"
+              />
+            </Form.Item>
+        </Col>
+        <Col xs={24} md={24} lg={12} xl={12} xxl={12}>
+        <Form.Item
+             className=" mb-8 px-4 text-white-50 custom-forminput custom-label pt-8 sc-error"
+              name="Last Name"
+              label="Last Name"
+              required
+              rules={[
+                {              
+                  required: true,
+                  message: "Is required",
+                },
+               
+                
+              ]}
+            >
+              <Input
+                className="cust-input"
+                maxLength={100}
+                placeholder="Wallet Address"
+              />
+            </Form.Item>
+        </Col>
+        <Col xs={24} md={24} lg={12} xl={12} xxl={12}>
+        <Form.Item
+             className=" mb-8 px-4 text-white-50 custom-forminput custom-label pt-8 sc-error"
+              name="Address"
+              label="Address"
+              required
+              rules={[
+                {              
+                  required: true,
+                  message: "Is required",
+                },
+               
+                
+              ]}
+            >
+              <Input
+                className="cust-input "
+                maxLength={100}
+                placeholder="Wallet Address"
+              />
+            </Form.Item>
         </Col>
        </Row>
        </>
@@ -70,7 +292,7 @@ const steps = [
   
     return (
       <div className='register-blockwid form-block'>
-        <Steps current={current}>
+        <Steps current={current} className='auth-steps'>
           {steps.map(item => (
             <Step key={item.title} title={item.title} />
           ))}
@@ -78,17 +300,17 @@ const steps = [
         <div>{steps[current].content}</div>
         <div>
           {current < steps.length - 1 && (
-            <Button type="primary" onClick={() => next()}>
+            <Button className="profile-sm-btn " type="primary" onClick={() => next()}>
               Next
             </Button>
           )}
           {current === steps.length - 1 && (
-            <Button type="primary" onClick={() => console.log('Processing complete!')}>
+            <Button className="profile-sm-btn " type="primary" onClick={() => console.log('Processing complete!')}>
               Next
             </Button>
           )}
           {current > 0 && (
-            <Button style={{ margin: '0 8px' }} onClick={() => prev()}>
+            <Button className="profile-sm-btn" style={{ margin: '0 8px' }} onClick={() => prev()}>
               Back
             </Button>
           )}
