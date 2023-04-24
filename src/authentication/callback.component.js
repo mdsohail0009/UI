09d3596fd89c4dc13b341b225b232d55aadb739d
SubbaReddy <subbaiah@tlvfintech.com>
2023-04-24 15:57:40 +0530

    call back

--- src/authentication/callback.component.js
+++ src/authentication/callback.component.js
@@ -6,30 +6,31 @@ const CallbackPage = () => {
     const { isLoading, error, isAuthenticated } = useAuth0()
     const handleSuccess = (user) => {
         this.handleRedirect(user)
-    }
-    const handleRedirect = (user) => {
-        this.props.updateProfile(user)
-        const url = localStorage.getItem("__url");
-        localStorage.removeItem("__url");
-        this.props.history.push(url && url !== "/callback" ? url : "/onboading")
-        this.props.cookies.set('SID', user.profile.sub, { path: '/', domain: process.env.REACT_APP_SUBDOMAIN || window.location.hostname })
-    }
+        useEffect(() => {
+            handleRedirect();
+        }, []);
+        const handleRedirect = (user) => {
+            this.props.updateProfile(user)
+            const url = localStorage.getItem("__url");
+            localStorage.removeItem("__url");
+            this.props.history.push(url && url !== "/callback" ? url : "/onboading")
+        }
 
-    return (
-        // <CallbackComponent
-        //     userManager={userManager}
-        //     successCallback={(user) => this.handleSuccess(user)}
-        //     errorCallback={error => {
-        //         if(error.message === "No matching state found in storage"){
-        //             window.location.replace(window.location.origin)
-        //         }else{
-        //             console.error(error.message);
-        //         }
-        //     }}
-        // >
-        <div className="loader">Loading .....ssssss</div>
-        // </CallbackComponent>
-    );
+        return (
+            // <CallbackComponent
+            //     userManager={userManager}
+            //     successCallback={(user) => this.handleSuccess(user)}
+            //     errorCallback={error => {
+            //         if(error.message === "No matching state found in storage"){
+            //             window.location.replace(window.location.origin)
+            //         }else{
+            //             console.error(error.message);
+            //         }
+            //     }}
+            // >
+            <div className="loader">Loading .....ssssss</div>
+            // </CallbackComponent>
+        );
 
-}
-export default CallbackPage;
+    }
+    export default CallbackPage;
