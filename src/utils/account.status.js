69b8d2722110a4376c9a3179d8f95ffa809f1186
Naresh <naresh@tlvfintech.com>
2023-05-19 19:38:28 +0530

    approval is in progress design changes

--- src/utils/account.status.js
+++ src/utils/account.status.js
@@ -39,10 +39,12 @@ class AccountStatus extends Component {
           
   
       ) : (
-        <div className='sumSub-confirm text-white text-center'><img src={pending} className="confirm-icon" alt={"success"} /><br />
+        <div className='sumSub-confirm text-white text-center'>
+          <div className="text-center">
+          <img src={pending} className="confirm-icon" alt={"success"} /><br />
           <span className='sumSub-review'>{this.props.customerState?.customerState === "Under Review" ? "Your account is under review state" : <>Your account approval is in progress.
             <p className="approve-email">
-              Contact our Customer success at <a href={`mailto: ${process.env.REACT_APP_ONBOARDING}`}>{process.env.REACT_APP_ONBOARDING}</a> for more information.</p></>}</span>
+              Contact our Customer success at <a href={`mailto: ${process.env.REACT_APP_ONBOARDING}`}>{process.env.REACT_APP_ONBOARDING}</a> for more information.</p></>}</span></div>
           {this.props.customerState?.customerState === "Under Review" && <p className='approve-email' style={{ wordBreak: 'break-all' }}> Please contact administrator</p>}
         </div>
       )}
