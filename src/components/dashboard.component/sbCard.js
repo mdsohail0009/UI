2da32151745a34b785156f2d9adb794bf0485cf8
Naresh <naresh@tlvfintech.com>
2023-06-02 19:42:56 +0530

    apply card changes

--- src/components/dashboard.component/sbCard.js
+++ src/components/dashboard.component/sbCard.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
-import { Typography } from 'antd';
-import logoWhite from "../../assets/images/SuisseBase.png";
+import { Typography,Button } from 'antd';
 import Translate from 'react-translate-component';
 import { withRouter } from 'react-router-dom';
 
-const { Text,Title } = Typography;
+const {Title } = Typography;
 class SbCard extends Component {
-    handleClick=()=>{
-        this.props.history.push(`/sbcard`)
+    handleApplyCard=()=>{
+        window.open(process.env.REACT_APP_ACCOUNT_USER_ONBOARD, '_blank')
     }
     render() {
         return (<>
              <Translate content="sb_card_title" component={Title} className="db-titles your-card" />
                 <div className='sb-card'>
-                    <div className="sb-innercard c-pointer" onClick={this.handleClick}>
+                    <div className="sb-innercard">
                     <div> 
-                        <img
-                        src={logoWhite}
-                        alt="logo"
-                        className="tlv-logo light  p-relative p-0 cardlogo-height"
-                        onClick={this.routeToHome}
-                        />
+                    
+                    <Button
+                          type="primary"
+                          className="custom-btn prime"
+                          style={{width:'140px'}}
+                          onClick={this.handleApplyCard}
+                        >
+                          Apply For A Card
+                        </Button>
                     </div>
-                <Text className="sb-text">1234 0404 2323 2443</Text>
+                  
                 </div>
                 </div>
         </>);
