d68dd452ad15886a540016f6a6027113df078444
Naresh <naresh@tlvfintech.com>
2023-06-30 19:57:53 +0530

    changes

--- src/components/addressbook.component/addressFiatView.js
+++ src/components/addressbook.component/addressFiatView.js
@@ -413,7 +413,7 @@ const AddressFiatView = (props) => {
 															</div>
 															</div>
 														</>}
-														 {(item.walletCode!=='CHF'&&item.walletCode!=='EUR'&& fiatAddress?.transferType !== "internationalIBAN")||fiatAddress?.transferType==="swifttransfer"&&
+														 {((item.walletCode!=='CHF'&&item.walletCode!=='EUR'&& fiatAddress?.transferType !== "internationalIBAN")||fiatAddress?.transferType==="swifttransfer")&&
 														 <div className="fait-box kpi-divstyle">
 															<Text className="kpi-label">
 															Bank Address 1
@@ -426,7 +426,7 @@ const AddressFiatView = (props) => {
 															</div>
 															</div>
 														}
-														{(item.walletCode!=='CHF'&&item.walletCode!=='EUR'&& fiatAddress?.transferType !== "internationalIBAN")||fiatAddress?.transferType==="swifttransfer"&&
+														{((item.walletCode!=='CHF'&&item.walletCode!=='EUR'&& fiatAddress?.transferType !== "internationalIBAN")||fiatAddress?.transferType==="swifttransfer")&&
 														<div className="fait-box kpi-divstyle">
 															<Text className="kpi-label">
 															Bank Address 2
