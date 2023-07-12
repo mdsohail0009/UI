a4df74478521050ecd9881b54006b2b8b8f971a4
Naresh <naresh@tlvfintech.com>
2023-07-12 10:29:10 +0530

    buy max value issue resolved

--- src/components/buy.component/buy.detail.js
+++ src/components/buy.component/buy.detail.js
@@ -104,7 +104,7 @@ class SelectCrypto extends Component {
 
         const { localValue, cryptoValue, isSwaped } = this.state.swapValues;
         const { buyMin, buyMax, coin, gbpInUsd, eurInUsd } = this.props.buyInfo?.selectedCoin?.data;
-        const _vaidator = validatePreview({ localValue, cryptValue: cryptoValue, wallet: this.state.selectedWallet, maxPurchase: buyMax, minPurchase: buyMin, gbpInUsd, eurInUsd })
+        const _vaidator = validatePreview({ localValue, cryptValue: cryptoValue, wallet: this.state.selectedWallet, maxPurchase: buyMax, minPurchase: buyMin, gbpInUsd, eurInUsd,coin:coin })
         if (!_vaidator.valid) {
             this.setState({ ...this.state, error: { ..._vaidator } });
             this.myRef.current.scrollIntoView(0,0);
